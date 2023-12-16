07e60f5bec1244ac0309183a91f5a09fabc47ff5
mkkukul <mkkukul@gmail.com>
2023-12-16 23:37:12 +0300

    7

--- 13. QuizApp/script.js
+++ 13. QuizApp/script.js
@@ -1,13 +1,16 @@
 function changeColor(element) {
-    element.style.backgroundColor = "lightblue"; /* Tıklandığında arka plan rengini değiştir */
+    element.style.backgroundColor = "lightblue";
+    element.style.color = "black";
 }
 
 document.querySelectorAll('.oval-box').forEach(item => {
     item.addEventListener('mouseover', function() {
-        this.style.backgroundColor = "lightgreen"; /* Üzerine gelindiğinde arka plan rengini değiştir */
+        this.style.backgroundColor = "lightgreen";
+        this.style.color = "black";
     });
 
     item.addEventListener('mouseout', function() {
-        this.style.backgroundColor = ""; /* Fare çekildiğinde arka plan rengini temizle */
+        this.style.backgroundColor = "";
+        this.style.color = "khaki";
     });
 });
