b26a94eefc05a53e61c3b744e11fe2da5054ae82
mkkukul <mkkukul@gmail.com>
2023-12-23 13:02:12 +0300

    8

--- 14. To Do List With Javascript  Step by Step Javascript Project/script.js
+++ 14. To Do List With Javascript  Step by Step Javascript Project/script.js
@@ -1,8 +1,12 @@
 document.guerySelector('#push').onclick = function 
 () {
-    if(document.querySelector('#newtask input')).
-    value.lenght == =){
+    if(document.querySelector('#newtask input').
+    value.lenght == 0){
+        alert("Please Enter a Task")
 
+    }
+    else{
+        document.querySelector('#tasks')
     }
     
 } 
