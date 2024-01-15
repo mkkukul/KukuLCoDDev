66cb94499bb96a1ac8a93ada1710137d31d51ff7
mkkukul <mkkukul@gmail.com>
2024-01-15 21:52:34 +0300

    c2.4

--- Coddig Artist/2 Product Filter and Search Using/script.js
+++ Coddig Artist/2 Product Filter and Search Using/script.js
@@ -50,4 +50,51 @@ let products = {
       },
     ],
   };
+ // products.data dizisindeki her ürün için döngü
+for (let i of products.data) {
+    // Yeni bir div elementi oluştur (kart)
+    let card = document.createElement("div");
+    
+    // Kart, kategoriye sahip olmalı ve başlangıçta gizli olmalıdır (hide sınıfı ile)
+    card.classList.add("card", i.category, "hide");
+    
+    // Resim container (image-container) div'i oluştur
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    
+    // Resim etiketi (img) oluştur
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    
+    // Resim etiketini resim container div'ine ekle
+    imgContainer.appendChild(image);
+    
+    // Kart elementine resim container div'ini ekle
+    card.appendChild(imgContainer);
   
+    // İçerik container (container) div'i oluştur
+    let container = document.createElement("div");
+    container.classList.add("container");
+    
+    // Ürün adı (productName) başlığı oluştur
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    
+    // Ürün adı başlığını içerik container div'ine ekle
+    container.appendChild(name);
+  
+    // Fiyat (price) başlığı oluştur
+    let price = document.createElement("h6");
+    price.innerText = "$" + i.price;
+    
+    // Fiyat başlığını içerik container div'ine ekle
+    container.appendChild(price);
+    
+    // Kart elementine içerik container div'ini ekle
+    card.appendChild(container);
+    
+    // "products" id'li HTML elementine kartı ekle
+    document.getElementById("products").appendChild(card);
+  }
+   
