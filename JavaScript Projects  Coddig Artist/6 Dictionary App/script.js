dd03aa06fe6d6115112a781bb46c60c6a4245e99
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-20 12:07:16 +0300

    23

--- JavaScript Projects  Coddig Artist/6 Dictionary App/script.js
+++ JavaScript Projects  Coddig Artist/6 Dictionary App/script.js
@@ -23,5 +23,8 @@ btn.addEventListener("click", () => {
         </p>
         <p class="word-example"> ${data[0].meanings[0].definitions[0].example || ""} </p>`;
         sound.setAttribute("src", `https:${ data[0].phonetics[0].audio }`);
+        .catch(() => {
+            result.innerHTML = `<
+        })
     }
 }
