1f9bddf362daa212cf9d33d7b42b917ee8bb9c33
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-20 11:46:18 +0300

    13

--- JavaScript Projects  Coddig Artist/6 Dictionary App/script.js
+++ JavaScript Projects  Coddig Artist/6 Dictionary App/script.js
@@ -1,3 +1,14 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result =  document.getElementById("result");
 const sound = document.getElementById("sound");
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+    .then((response) => response.json())
+    .then(data => {
+        console.log(data);
+        result.innerHTML = `
+        <div class="word">
+            <h3>${in}
+    }
+}
