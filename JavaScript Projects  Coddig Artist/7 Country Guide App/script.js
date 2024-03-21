7ab9c01611b68c5c5f768eb5717ec54f3914f0cc
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-21 15:40:28 +0300

    Açıklamaları ile 26 commit

--- JavaScript Projects  Coddig Artist/7 Country Guide App/script.js
+++ JavaScript Projects  Coddig Artist/7 Country Guide App/script.js
@@ -1,22 +1,25 @@
+// Arama butonunu alır
 let searchBtn = document.getElementById("search-btn");
+
+// Ülke adı giriş alanını alır
 let countryInp = document.getElementById("country-inp");
+
+// Arama butonuna tıklama olayını dinler
 searchBtn.addEventListener("click", () => {
+  // Kullanıcının girdiği ülke adını alır
   let countryName = countryInp.value;
+
+  // API'nin son URL'sini oluşturur
   let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+
+  // Son URL'yi konsola yazdırır
   console.log(finalURL);
+
+  // API'ye bir GET isteği gönderir ve gelen yanıtı işler
   fetch(finalURL)
-    .then((response) => response.json())
-    .then((data) => {
-      //   console.log(data[0]);
-      //   console.log(data[0].capital[0]);
-      //   console.log(data[0].flags.svg);
-      //   console.log(data[0].name.common);
-      //   console.log(data[0].continents[0]);
-      //   console.log(Object.keys(data[0].currencies)[0]);
-      //   console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-      //   console.log(
-      //     Object.values(data[0].languages).toString().split(",").join(", ")
-      //   );
+    .then((response) => response.json()) // Yanıtı JSON'a dönüştürür
+    .then((data) => { // JSON verilerini işler
+      // Sonuçları HTML içine yerleştirir
       result.innerHTML = `
         <img src="${data[0].flags.svg}" class="flag-img">
         <h2>${data[0].name.common}</h2>
@@ -57,7 +60,7 @@ searchBtn.addEventListener("click", () => {
         </div>
       `;
     })
-    .catch(() => {
+    .catch(() => { // Hata durumunda bir hata mesajı görüntüler
       if (countryName.length == 0) {
         result.innerHTML = `<h3>The input field cannot be empty</h3>`;
       } else {
