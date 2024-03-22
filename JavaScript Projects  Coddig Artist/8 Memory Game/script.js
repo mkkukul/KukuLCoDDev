e0e53bb4ad58163c6ea3e66b56bc5ad1550dd62b
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-22 12:41:26 +0300

    14

--- JavaScript Projects  Coddig Artist/8 Memory Game/script.js
+++ JavaScript Projects  Coddig Artist/8 Memory Game/script.js
@@ -1,3 +1,3 @@
 const moves = document.getElementById( "moves-count" );
 const timeValue = document.getElementById("time");
-const startButton = document
+const startButton = document.getElementById("start");
