e81d7a7508744b8b51484b42f079b0d86bc8152c
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-23 21:04:17 +0300

    25

--- JavaScript Projects  Coddig Artist/9 Double Range Slider/script.js
+++ JavaScript Projects  Coddig Artist/9 Double Range Slider/script.js
@@ -1,9 +1,9 @@
 window.onload = function(){
     slideOne();
-    slideOne();
+    slideTwo();
 }
 
-let sliderOne = document.getElementById( "slider-1" );
+let sliderOne = document.getElementById("slider-1");
 let sliderTwo = document.getElementById("slider-2");
 let displayValOne = document.getElementById("range1");
 let displayValTwo = document.getElementById("range2");
@@ -12,7 +12,15 @@ let sliderTrack = document.querySelector(".slider-track");
 let sliderMaxValue = document.getElementById("slider-1").max;
 
 function slideOne(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value)<=  minGap){sliderTwo.value = parseInt(sliderOne.value) + minGap;
+    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+        sliderOne.value = parseInt(sliderTwo.value) - minGap;
+    }
+    displayValOne.textContent = sliderOne.value;
+    fillColor();
+}
+function slideTwo(){
+    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+        sliderTwo.value = parseInt(sliderOne.value) + minGap;
     }
     displayValTwo.textContent = sliderTwo.value;
     fillColor();
