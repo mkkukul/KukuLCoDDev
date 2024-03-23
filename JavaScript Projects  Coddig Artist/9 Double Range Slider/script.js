2a317e9c88a6c53293d2da4703652e32f51c598b
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-23 20:54:53 +0300

    14

--- JavaScript Projects  Coddig Artist/9 Double Range Slider/script.js
+++ JavaScript Projects  Coddig Artist/9 Double Range Slider/script.js
@@ -5,4 +5,5 @@ window.onload = function(){
 
 let sliderOne = document.getElementById( "slider-1" );
 let sliderTwo = document.getElementById("slider-2");
-let displayValOne = 
+let displayValOne = document.getElementById("range1");
+let displayValTwo = document.getElementById("range2"); 
