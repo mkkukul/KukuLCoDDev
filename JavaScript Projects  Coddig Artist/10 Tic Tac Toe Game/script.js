78d8dcfd8cdc604fe3ba4e6a08222b0e50230e36
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-25 10:50:47 +0300

    12

--- JavaScript Projects  Coddig Artist/10 Tic Tac Toe Game/script.js
+++ JavaScript Projects  Coddig Artist/10 Tic Tac Toe Game/script.js
@@ -1,3 +1,4 @@
 let btnRef = document.querySelectorAll(".button-option");
 let popupRef = document.querySelector(".popop");
 let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
