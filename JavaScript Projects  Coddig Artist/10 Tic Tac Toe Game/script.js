59825aff336d5788b2042b293b2b95dfe16b2b55
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-25 10:54:48 +0300

    16

--- JavaScript Projects  Coddig Artist/10 Tic Tac Toe Game/script.js
+++ JavaScript Projects  Coddig Artist/10 Tic Tac Toe Game/script.js
@@ -7,4 +7,10 @@ let msgRef = document.getElementById("message");
 let winnigPattern = [
     [0, 1, 2],
     [0, 3, 6],
+    [2, 5, 8],
+    [6, 7, 8],
+    [3, 4, 5],
+    [1, 4, 7],
+    [0, 4, 8],
+    [2, 4, 6],
 ]
