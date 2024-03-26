af04ece8978d05da8c5425af600240dba9a9962d
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-26 09:33:53 +0300

    23

--- JavaScript Projects  Coddig Artist/11 Roll The Dice/script.js
+++ JavaScript Projects  Coddig Artist/11 Roll The Dice/script.js
@@ -10,14 +10,18 @@ function roll(){
     dice.forEach(function(die){
         die.classList.add("shake";)
     });
-    setTimeout(function(die){
-        die.forEach(function(die){
-        die.classList.remove("shake");
-    });
+    setTimeout(function(){
+        dice.forEach(function(die){
+            die.classList.remove("shake");
+        });
         let dieOneValue = mat.floor(math.random()*6);
         let dieTwoValue = math.floor(math.random()*6);
         console.log(dieOneValue, dieTwoValue);
         document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
         document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
         document.querySelector("#total").innerHTML = "Your roll is" + ((dieOneValue+1)+ (dieTwoValue+1));
+    }, 
+    1000
+    );
 }
+roll();
