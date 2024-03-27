c322c749d1cb81c7a68ebce952ed75c69b710abe
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-27 11:27:03 +0300

    8

--- JavaScript Projects  Coddig Artist/13 To Do List With Local Storage/script.js
+++ JavaScript Projects  Coddig Artist/13 To Do List With Local Storage/script.js
@@ -1,1 +1,5 @@
 const newTaskInput = document.querySelector("#new-task input");
+const tasksDiv = document.querySelector("#tasks");
+let deleteTasks, editTasks, tasks;
+let updateNote = "";
+let count;
