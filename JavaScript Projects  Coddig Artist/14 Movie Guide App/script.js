0f219096bf15aa39478255b1fef7f41e37977994
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-28 10:44:57 +0300

    18

--- JavaScript Projects  Coddig Artist/14 Movie Guide App/script.js
+++ JavaScript Projects  Coddig Artist/14 Movie Guide App/script.js
@@ -19,9 +19,26 @@ let getMovie =() => {
             if(data.Response == "True"){
                 result.innerHTML = `
                 <div class="info">
-                
+                    <img src=${data.Poster} class="poster">
+                    <div>
+                        <h2>${data.Title}</h2>
+                        <div class="rating">
+                            <img src="star-icon.svg">
+                            <h4>${data.imdbRating}</h4>
+                        </div>
+                        <div class="details">
+                            <span>${data.Rated}</span>
+                            <span>${data.Year}</span>
+                            <span>${data.Runtime}</span>
+                        </div>
+                        <div class="genre">
+                            <div>
+                                ${data.Genre.split(",").join("</div><div>")}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                `
+                `;
 
             }
         }
