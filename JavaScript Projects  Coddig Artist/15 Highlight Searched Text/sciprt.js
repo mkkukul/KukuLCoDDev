06bb602abebdf9145d7708f351c5bff9758fdb19
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-29 10:06:00 +0300

    The And

--- JavaScript Projects  Coddig Artist/15 Highlight Searched Text/sciprt.js
+++ JavaScript Projects  Coddig Artist/15 Highlight Searched Text/sciprt.js
@@ -5,3 +5,20 @@ function search(){
     let pattern = new RegExp(`${textToSearch}`,"gi");
     paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`) 
 }
+//! function search() {
+    // Aranacak metni alır
+    //! let textToSearch = document.getElementById("text-to-search").value;
+    // 
+    // Paragrafı seçer
+    //! let paragraph = document.getElementById("paragraph");
+    // 
+    // Metin içindeki özel karakterleri metin deseni için kaçış karakteri ile değiştirir
+    //! textToSearch =  textToSearch.replace(/[.*+?^${}|[\]\\]/g,"\\$&"); 
+    // 
+    // Arama yapmak için düzenli ifade deseni oluşturur
+    //! let pattern = new RegExp(`${textToSearch}`, "gi");
+    // 
+    // Paragraftaki metni, desene uyan her eşleşme için bir <mark> elementiyle değiştirir
+    //! paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`); 
+//! }
+// 
