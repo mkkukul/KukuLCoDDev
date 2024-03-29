4fe25f97d5e9f44012eafba707177fa93ed25cec
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-29 09:30:37 +0300

    22

--- JavaScript Projects  Coddig Artist/15 Highlight Searched Text/sciprt.js
+++ JavaScript Projects  Coddig Artist/15 Highlight Searched Text/sciprt.js
@@ -3,5 +3,5 @@ function search(){
     let paragraph = document.getElementById("paragraph");
     textToSearch =  textToSearch.replace(/[.*+?^${}|[\]\\]/g,"\\$&"); 
     let pattern = new RegExp(`${textToSearch}`,"gi");
-    paragraph 
+    paragraph.innerHTML = paragraph 
 }
