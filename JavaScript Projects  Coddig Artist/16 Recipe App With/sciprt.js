1cf627796c3154482b88a428d29a0f4122c2690e
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-30 11:33:35 +0300

    12

--- JavaScript Projects  Coddig Artist/16 Recipe App With/sciprt.js
+++ JavaScript Projects  Coddig Artist/16 Recipe App With/sciprt.js
@@ -4,5 +4,8 @@ let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 searchBtn.addEventListener( "click",()=> {
     let userInp = document.getElementById("user-inp").value;
     if(userInp.length == 0){
-        result
+        result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
+    }else {
+        
+    }
 } );
