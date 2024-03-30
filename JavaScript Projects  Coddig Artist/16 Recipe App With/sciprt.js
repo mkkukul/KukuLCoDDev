433589fe64990c7f8f0c74439d7e937c1e766cf3
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-30 11:29:41 +0300

    9

--- JavaScript Projects  Coddig Artist/16 Recipe App With/sciprt.js
+++ JavaScript Projects  Coddig Artist/16 Recipe App With/sciprt.js
@@ -1,3 +1,4 @@
 let  result = document.getElementById("result");
 let searchBtn = document.getElementById("#search-btn");
 let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+searchBtn.addEventListener( "click",() );
