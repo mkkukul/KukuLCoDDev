f48cfc2c2a193e5add24a4dcd5b06430730440bd
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-30 11:38:06 +0300

    13

--- JavaScript Projects  Coddig Artist/16 Recipe App With/sciprt.js
+++ JavaScript Projects  Coddig Artist/16 Recipe App With/sciprt.js
@@ -6,6 +6,6 @@ searchBtn.addEventListener( "click",()=> {
     if(userInp.length == 0){
         result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
     }else {
-        
+        fetch()
     }
 } );
