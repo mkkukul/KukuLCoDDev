ced1b2e8e2b3f5dda8eebbc10b1ae006debb221d
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-03-31 20:06:40 +0300

    8

--- JavaScript Projects  Coddig Artist/17 Age Calculator/sciprt.js
+++ JavaScript Projects  Coddig Artist/17 Age Calculator/sciprt.js
@@ -2,5 +2,5 @@ const months = [31,28,31,30,31,30,31,31,30,31,30,31];
 
 function ageCalculate(){
     let today = new Date();
-    
+    let inputDate = new Date(document.getElementById("date-input").value);
 }
