2b4e0f541196203cc5712df9e293b9c53142a466
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-04-01 22:16:24 +0300

    9

--- JavaScript Projects  Coddig Artist/18 Number Guessing Game/sciprt.js
+++ JavaScript Projects  Coddig Artist/18 Number Guessing Game/sciprt.js
@@ -1,3 +1,5 @@
 var msg1 = document.getElementById("message1");
 var msg2 = document.getElementById("message2");
 var msg3 = document.getElementById("message3");
+
+var answer = Math.floor(Math.random()*100) + 1;
