a6a4b0aaba8baa4f230ca0d5e428be8d112bd87e
mkkukul <98874513+mkkukul@users.noreply.github.com>
2024-04-02 10:46:39 +0300

    10

--- JavaScript Projects  Coddig Artist/20 Calculator Using/sciprt.js
+++ JavaScript Projects  Coddig Artist/20 Calculator Using/sciprt.js
@@ -3,4 +3,5 @@ let button_input = document.querySelectorAll(".input-button");
 let input = document.getElementById("input");
 let equal = document.getElementById("equal");
 let clear =  document.getElementById("clear");
+let erase = document.getElementById( "erase" );
 
