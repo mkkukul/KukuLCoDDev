431ca7f5b8b6bda04a95283771dd86c932b40d65
mkkukul <mkkukul@gmail.com>
2024-04-05 09:53:18 +0300

    20

--- JavaScript Projects  Coddig Artist/21 Pokemon Card/sciprt.js
+++ JavaScript Projects  Coddig Artist/21 Pokemon Card/sciprt.js
@@ -1,19 +1,18 @@
 const typeColor = {
-    bug:"#26de81",
-    dragon:"#ffeaa7",
-    electric:"#fed330",
-    fairy:"#FF0069",
+    bug: "#26de81",
+    dragon: "#ffeaa7",
+    electric: "#fed330",
+    fairy: "#FF0069",
     fighting: "#30336b",
-    fire:"#f0932b",
-    flying:"#81ecec",
+    fire: "#f0932b",
+    flying: "#81ecec",
     grass: "#00b894",
-    ground:"#EFB549",
+    ground: "#EFB549",
     ghost: "#a55eea",
-    ice:"#74b9ff",
-    normal:"#95afc0",
-    poison : "#6c5ce7",
-    psychic:"#a29bfe",
-
-
-
-}
+    ice: "#74b9ff",
+    normal: "#95afc0",
+    poison: "#6c5ce7",
+    psychic: "#a29bfe",
+    rock: "#2d3436",
+    water: "#0190FF",
+};
