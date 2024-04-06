ff5291702027796f1c683da9d14c03ea9bd8dd26
mkkukul <mkkukul@gmail.com>
2024-04-06 18:31:22 +0300

    23

--- JavaScript Projects  Coddig Artist/22 Budget App/sciprt.js
+++ JavaScript Projects  Coddig Artist/22 Budget App/sciprt.js
@@ -77,3 +77,26 @@ const listCreator = (expenseName, expenseValue) => {
     sublistContent.appendChild(deleteButton);
     document.getElementById("list").appendChild(sublistContent);
 };
+//Function To Add Expenses
+checkAmountButton.addEventListener("click", () => {
+    //empty checks
+    if (!userAmount.value || !productTitle.value) {
+      productTitleError.classList.remove("hide");
+      return false;
+    }
+    //Enable buttons
+    disableButtons(false);
+    //Expense
+    let expenditure = parseInt(userAmount.value);
+    //Total expense (existing + new)
+    let sum = parseInt(expenditureValue.innerText) + expenditure;
+    expenditureValue.innerText = sum;
+    //Total balance(budget - total expense)
+    const totalBalance = tempAmount - sum;
+    balanceValue.innerText = totalBalance;
+    //Create list
+    listCreator(productTitle.value, userAmount.value);
+    //Empty inputs
+    productTitle.value = "";
+    userAmount.value = "";
+  });
