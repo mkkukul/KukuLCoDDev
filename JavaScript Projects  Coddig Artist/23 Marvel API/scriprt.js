226d3a205dc000fca0b39eafecb16af8246a16c7
mkkukul <mkkukul@gmail.com>
2024-04-07 16:34:48 +0300

    16

--- JavaScript Projects  Coddig Artist/23 Marvel API/scriprt.js
+++ JavaScript Projects  Coddig Artist/23 Marvel API/scriprt.js
@@ -12,3 +12,24 @@ function displayWords(value) {
 function removeElements() {
   listContainer.innerHTML = "";
 }
+
+input.addEventListener("keyup", async () => {
+    removeElements();
+    if (input.value.length < 4) {
+      return false;
+    }
+    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&nameStartsWith=${input.value}`;
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    jsonData.data["results"].forEach((result) => {
+      let name = result.name;
+      let div = document.createElement("div");
+      div.style.cursor = "pointer";
+      div.classList.add("autocomplete-items");
+      div.setAttribute("onclick", "displayWords('" + name + "')");
+      let word = "<b>" + name.substr(0, input.value.length) + "</b>";
+      word += name.substr(input.value.length);
+      div.innerHTML = `<p class="item">${word}</p>`;
+      listContainer.appendChild(div);
+    });
+  });
