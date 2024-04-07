4d16770bca39f4969c70bf5b2936a52fe256e9e5
mkkukul <mkkukul@gmail.com>
2024-04-07 16:37:16 +0300

    18

--- JavaScript Projects  Coddig Artist/23 Marvel API/scriprt.js
+++ JavaScript Projects  Coddig Artist/23 Marvel API/scriprt.js
@@ -33,3 +33,25 @@ input.addEventListener("keyup", async () => {
       listContainer.appendChild(div);
     });
 });
+button.addEventListener(
+    "click",
+    (getRsult = async () => {
+      if (input.value.trim().length < 1) {
+        alert("Input cannot be blank");
+      }
+      showContainer.innerHTML = "";
+      const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&name=${input.value}`;
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      jsonData.data["results"].forEach((element) => {
+        showContainer.innerHTML = `<div class="card-container">
+          <div class="container-character-image">
+          <img src="${
+            element.thumbnail["path"] + "." + element.thumbnail["extension"]
+          }"/></div>
+          <div class="character-name">${element.name}</div>
+          <div class="character-description">${element.description}</div>
+          </div>`;
+      });
+    })
+  );
