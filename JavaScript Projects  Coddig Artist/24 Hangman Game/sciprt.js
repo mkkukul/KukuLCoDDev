99f8cf531b642fbe045f20f74d43a251dbe3cbc7
mkkukul <mkkukul@gmail.com>
2024-04-08 22:43:19 +0300

    16

---
+++ JavaScript Projects  Coddig Artist/24 Hangman Game/sciprt.js
@@ -0,0 +1,8 @@
+//Initial References
+const letterContainer = document.getElementById("letter-container");
+const optionsContainer = document.getElementById("options-container");
+const userInputSection = document.getElementById("user-input-section");
+const newGameContainer = document.getElementById("new-game-container");
+const newGameButton = document.getElementById("new-game-button");
+const canvas = document.getElementById("canvas");
+const resultText = document.getElementById("result-text");
