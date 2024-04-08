1c3597c997922999b3b76e10aa34232b5c74938e
mkkukul <mkkukul@gmail.com>
2024-04-08 22:48:03 +0300

    24

--- JavaScript Projects  Coddig Artist/24 Hangman Game/sciprt.js
+++ JavaScript Projects  Coddig Artist/24 Hangman Game/sciprt.js
@@ -57,6 +57,8 @@ const blocker = () => {
     });
     newGameContainer.classList.remove("hide");
 };
+
+
 //Word Generator
 const generateWord = (optionValue) => {
     let optionsButtons = document.querySelectorAll(".options");
@@ -78,4 +80,64 @@ const generateWord = (optionValue) => {
     let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
     //Display each element as span
     userInputSection.innerHTML = displayItem;
+};
+
+//Initial Function (Called when page loads/user presses new game)
+const initializer = () => {
+    winCount = 0;
+    count = 0;
+    //Initially erase all content and hide letteres and new game button
+    userInputSection.innerHTML = "";
+    optionsContainer.innerHTML = "";
+    letterContainer.classList.add("hide");
+    newGameContainer.classList.add("hide");
+    letterContainer.innerHTML = "";
+    //For creating letter buttons
+    for (let i = 65; i < 91; i++) {
+      let button = document.createElement("button");
+      button.classList.add("letters");
+      //Number to ASCII[A-Z]
+      button.innerText = String.fromCharCode(i);
+      //character button click
+      button.addEventListener("click", () => {
+        let charArray = chosenWord.split("");
+        let dashes = document.getElementsByClassName("dashes");
+        //if array contains clciked value replace the matched dash with letter else dram on canvas
+        if (charArray.includes(button.innerText)) {
+          charArray.forEach((char, index) => {
+            //if character in array is same as clicked button
+            if (char === button.innerText) {
+              //replace dash with letter
+              dashes[index].innerText = char;
+              //increment counter
+              winCount += 1;
+              //if winCount equals word lenfth
+              if (winCount == charArray.length) {
+                resultText.innerHTML = `<h2 class='win-msg'>You Win!!</h2><p>The word was <span>${chosenWord}</span></p>`;
+                //block all buttons
+                blocker();
+              }
+            }
+          });
+        } else {
+          //lose count
+          count += 1;
+          //for drawing man
+          drawMan(count);
+          //Count==6 because head,body,left arm, right arm,left leg,right leg
+          if (count == 6) {
+            resultText.innerHTML = `<h2 class='lose-msg'>You Lose!!</h2><p>The word was <span>${chosenWord}</span></p>`;
+            blocker();
+          }
+        }
+        //disable clicked button
+        button.disabled = true;
+      });
+      letterContainer.append(button);
+    }
+    displayOptions();
+    //Call to canvasCreator (for clearing previous canvas and creating initial canvas)
+    let { initialDrawing } = canvasCreator();
+    //initialDrawing would draw the frame
+    initialDrawing();
 };
