1afdb94b142670b5a2e478f7df5a1f8617f70560
mkkukul <mkkukul@gmail.com>
2024-04-09 20:39:21 +0300

    14

---
+++ JavaScript Projects  Coddig Artist/25 News App/script.js
@@ -0,0 +1,4 @@
+//API Used: http://newsapi.org/s/india-news-api
+const container = document.querySelector(".container");
+const optionsContainer = document.querySelector(".options-container");
+// "in" stands for India
