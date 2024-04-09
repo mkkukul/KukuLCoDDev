8224d0b408476d8085edf8e515f2135224e0d942
mkkukul <mkkukul@gmail.com>
2024-04-09 20:40:06 +0300

    16

--- JavaScript Projects  Coddig Artist/25 News App/script.js
+++ JavaScript Projects  Coddig Artist/25 News App/script.js
@@ -11,3 +11,26 @@ const options = [
   "sports",
   "technology",
 ];
+//100 requests per day
+let requestURL;
+
+//Create cards from data
+const generateUI = (articles) => {
+    for (let item of articles) {
+      let card = document.createElement("div");
+      card.classList.add("news-card");
+      card.innerHTML = `<div class="news-image-container">
+      <img src="${item.urlToImage || "./newspaper.jpg"}" alt="" />
+      </div>
+      <div class="news-content">
+        <div class="news-title">
+          ${item.title}
+        </div>
+        <div class="news-description">
+        ${item.description || item.content || ""}
+        </div>
+        <a href="${item.url}" target="_blank" class="view-button">Read More</a>
+      </div>`;
+      container.appendChild(card);
+    }
+};
