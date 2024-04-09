18e67a838d35baf3da50b71e9a775f5bdef129e6
mkkukul <mkkukul@gmail.com>
2024-04-09 20:39:37 +0300

    15

--- JavaScript Projects  Coddig Artist/25 News App/script.js
+++ JavaScript Projects  Coddig Artist/25 News App/script.js
@@ -2,3 +2,12 @@
 const container = document.querySelector(".container");
 const optionsContainer = document.querySelector(".options-container");
 // "in" stands for India
+const country = "in";
+const options = [
+  "general",
+  "entertainment",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
