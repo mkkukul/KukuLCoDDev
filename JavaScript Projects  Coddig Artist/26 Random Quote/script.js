818023bdfd67fce5f69fcee5df2ed3ecd6c0a62a
mkkukul <mkkukul@gmail.com>
2024-04-10 22:53:46 +0300

    15

--- JavaScript Projects  Coddig Artist/26 Random Quote/script.js
+++ JavaScript Projects  Coddig Artist/26 Random Quote/script.js
@@ -5,14 +5,13 @@ let btn = document.getElementById("btn");
 const url = "https://api.quotable.io/random";
 
 let getQuote = () => {
-    fetch(url)
-      .then((data) => data.json())
-      .then((item) => {
-        quote.innerText = item.content;
-        author.innerText = item.author;
-      });
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      quote.innerText = item.content;
+      author.innerText = item.author;
+    });
 };
 
-
 window.addEventListener("load", getQuote);
-btn.addEventListener("click", getQuote);  
+btn.addEventListener("click", getQuote);
