fecababfac7ce73a8d9ed6bdfab898c0776acb74
mkkukul <mkkukul@gmail.com>
2024-04-11 21:53:43 +0300

    18

--- JavaScript Projects  Coddig Artist/27 Currency Converter/script.js
+++ JavaScript Projects  Coddig Artist/27 Currency Converter/script.js
@@ -22,3 +22,25 @@ currencies.forEach((currency) => {
 fromDropDown.value = "USD";
 toDropDown.value = "INR";
 
+
+let convertCurrency = () => {
+    //Create References
+    const amount = document.querySelector("#amount").value;
+    const fromCurrency = fromDropDown.value;
+    const toCurrency = toDropDown.value;
+    //If amount input field is not empty
+    if (amount.length != 0) {
+      fetch(api)
+        .then((resp) => resp.json())
+        .then((data) => {
+          let fromExchangeRate = data.conversion_rates[fromCurrency];
+          let toExchangeRate = data.conversion_rates[toCurrency];
+          const convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
+          result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(
+            2
+          )} ${toCurrency}`;
+        });
+    } else {
+      alert("Please fill in the amount");
+    }
+  };
