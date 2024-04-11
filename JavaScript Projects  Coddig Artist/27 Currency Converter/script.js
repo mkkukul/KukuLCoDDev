e9489af3eb2f13edd21daa4c50655e3465ee0887
mkkukul <mkkukul@gmail.com>
2024-04-11 21:52:10 +0300

    10

--- JavaScript Projects  Coddig Artist/27 Currency Converter/script.js
+++ JavaScript Projects  Coddig Artist/27 Currency Converter/script.js
@@ -1,3 +1,11 @@
 let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
 const fromDropDown = document.getElementById("from-currency-select");
 const toDropDown = document.getElementById("to-currency-select");
+
+//Create dropdown from the currencies array
+currencies.forEach((currency) => {
+    const option = document.createElement("option");
+    option.value = currency;
+    option.text = currency;
+    fromDropDown.add(option);
+  });
