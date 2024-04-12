6bfd1312a2ce59ba174fa1fbd5c25b014d110a33
mkkukul <mkkukul@gmail.com>
2024-04-12 21:37:02 +0300

    7

---
+++ JavaScript Projects  Coddig Artist/28 Typing Test/script.js
@@ -0,0 +1,3 @@
+const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
+const quoteSection = document.getElementById("quote");
+const userInput = document.getElementById("quote-input");
