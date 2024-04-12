424fadf8625317e92675187b54916ef850d403af
mkkukul <mkkukul@gmail.com>
2024-04-12 21:38:17 +0300

    10

--- JavaScript Projects  Coddig Artist/28 Typing Test/script.js
+++ JavaScript Projects  Coddig Artist/28 Typing Test/script.js
@@ -7,3 +7,19 @@ let time = 60;
 let timer = "";
 let mistakes = 0;
 
+/Display random quotes
+const renderNewQuote = async () => {
+  //Fetch contents from url
+  const response = await fetch(quoteApiUrl);
+  //Store response
+  let data = await response.json();
+  //Access quote
+  quote = data.content;
+  //Array of characters in the quote
+  let arr = quote.split("").map((value) => {
+    //wrap the characters in a span tag
+    return "<span class='quote-chars'>" + value + "</span>";
+  });
+  //join array for displaying
+  quoteSection.innerHTML += arr.join("");
+};
