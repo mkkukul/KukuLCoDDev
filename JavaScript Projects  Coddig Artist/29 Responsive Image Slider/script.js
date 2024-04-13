00d2d901919d7c8ae6ff6e3a37244822f74b7280
mkkukul <mkkukul@gmail.com>
2024-04-13 06:53:34 +0300

    9

--- JavaScript Projects  Coddig Artist/29 Responsive Image Slider/script.js
+++ JavaScript Projects  Coddig Artist/29 Responsive Image Slider/script.js
@@ -1,2 +1,5 @@
 let i = 0; // current slide
 let j = 4; // total slides
+
+const dots = document.querySelectorAll(".dot-container button");
+const images = document.querySelectorAll(".image-container img");
