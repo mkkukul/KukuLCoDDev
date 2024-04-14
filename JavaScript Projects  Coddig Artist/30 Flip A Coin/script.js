de06a551a906db92e972f78c30b1d800d8ef7a32
mkkukul <mkkukul@gmail.com>
2024-04-14 22:48:45 +0300

    11

--- JavaScript Projects  Coddig Artist/30 Flip A Coin/script.js
+++ JavaScript Projects  Coddig Artist/30 Flip A Coin/script.js
@@ -4,3 +4,21 @@ let coin = document.querySelector(".coin");
 let flipBtn = document.querySelector("#flip-button");
 let resetBtn = document.querySelector("#reset-button");
 
+flipBtn.addEventListener("click", () => {
+    let i = Math.floor(Math.random() * 2);
+    coin.style.animation = "none";
+    if(i){
+        setTimeout(function(){
+            coin.style.animation = "spin-heads 3s forwards";
+        }, 100);
+        heads++;
+    }
+    else{
+        setTimeout(function(){
+            coin.style.animation = "spin-tails 3s forwards";
+        }, 100);
+        tails++;
+    }
+    setTimeout(updateStats, 3000);
+    disableButton();
+});
