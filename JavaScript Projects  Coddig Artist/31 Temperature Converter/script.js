a1cd2b828743b663301e884d22c5e2838629fd89
mkkukul <mkkukul@gmail.com>
2024-04-15 10:35:54 +0300

    26

--- JavaScript Projects  Coddig Artist/31 Temperature Converter/script.js
+++ JavaScript Projects  Coddig Artist/31 Temperature Converter/script.js
@@ -2,12 +2,12 @@ let celsius = document.getElementById("celsius");
 let fahrenheit = document.getElementById("fahrenheit");
 
 function celToFar(){
-    let output = (parseFloat(celsius.value) * 9/5 ) + 32;
+    let output = ( parseFloat(celsius.value) * 9/5 ) + 32;
     fahrenheit.value = parseFloat(output.toFixed(2));
 }
 
 function farToCel(){
-    let output = (parseFloat(fahrenheit.value) - 32) * 5/9;
-    celsius.value = parseFloat(output.toFixed(2));
+    let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
+    celsius.value = parseFloat( output.toFixed(2));
     console.log(output);
 }
