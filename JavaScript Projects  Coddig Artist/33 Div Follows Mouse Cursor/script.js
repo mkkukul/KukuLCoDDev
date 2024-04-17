6b310cc02932c8c6469a557f08995f8703d60f62
mkkukul <mkkukul@gmail.com>
2024-04-17 21:41:25 +0300

    17

--- JavaScript Projects  Coddig Artist/33 Div Follows Mouse Cursor/script.js
+++ JavaScript Projects  Coddig Artist/33 Div Follows Mouse Cursor/script.js
@@ -9,3 +9,7 @@ function isTouchDevice() {
     return false;
   }
 }
+
+const move = (e) => {
+    //Try, catch to avoid any errors for touch screens (Error thrown when user doesn't move his finger)
+    try {
