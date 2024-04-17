5ee8780926d722ae54ed5544a7b68838f5b62d58
mkkukul <mkkukul@gmail.com>
2024-04-17 21:42:10 +0300

    19

--- JavaScript Projects  Coddig Artist/33 Div Follows Mouse Cursor/script.js
+++ JavaScript Projects  Coddig Artist/33 Div Follows Mouse Cursor/script.js
@@ -14,5 +14,9 @@ const move = (e) => {
     //Try, catch to avoid any errors for touch screens (Error thrown when user doesn't move his finger)
     try {
         //PageX and PageY return the position of client's cursor from top left of screen
-    var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
-    var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+        var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+        var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+    } catch (e) {}
+    //set left and top of div based on mouse position
+    myDiv.style.left = x - 50 + "px";
+    myDiv.style.top = y - 50 + "px";
