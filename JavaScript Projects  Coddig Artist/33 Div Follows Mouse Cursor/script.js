5b22b1c8158016e6909c503c90be98bde5bdf0c1
mkkukul <mkkukul@gmail.com>
2024-04-17 21:39:55 +0300

    15

--- JavaScript Projects  Coddig Artist/33 Div Follows Mouse Cursor/script.js
+++ JavaScript Projects  Coddig Artist/33 Div Follows Mouse Cursor/script.js
@@ -2,5 +2,6 @@ let myDiv = document.getElementById("my-div");
 //Detect touch device
 function isTouchDevice() {
     try {
-        //We try to create TouchEvent. It would fail for desktops and throw error
+    //We try to create TouchEvent. It would fail for desktops and throw error
     document.createEvent("TouchEvent");
+    return true;
