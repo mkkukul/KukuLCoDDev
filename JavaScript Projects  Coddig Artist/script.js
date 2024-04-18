b0769ff26c4781de0186c4f17549db68282348ad
mkkukul <mkkukul@gmail.com>
2024-04-18 20:44:39 +0300

    18

--- JavaScript Projects  Coddig Artist/script.js
+++ JavaScript Projects  Coddig Artist/script.js
@@ -3,3 +3,5 @@ var result = document.getElementById("result");
 var limit = 60;
 result.textContent = 0 + "/" + limit;
 myText.addEventListener("input",function(){
+    var textLength = myText.value.length;
+    result.textContent = textLength + "/" + limit; 
