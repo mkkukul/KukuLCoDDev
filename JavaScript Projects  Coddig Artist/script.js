853912a249bbfb11725348f737626bc432440f6b
mkkukul <mkkukul@gmail.com>
2024-04-18 20:45:07 +0300

    17

--- JavaScript Projects  Coddig Artist/script.js
+++ JavaScript Projects  Coddig Artist/script.js
@@ -4,4 +4,13 @@ var limit = 60;
 result.textContent = 0 + "/" + limit;
 myText.addEventListener("input",function(){
     var textLength = myText.value.length;
-    result.textContent = textLength + "/" + limit; 
+    result.textContent = textLength + "/" + limit;
+    
+    if(textLength > limit){
+        myText.style.borderColor = "#ff2851";
+        result.style.color = "#ff2851";
+    }
+    else{
+        myText.style.borderColor = "#b2b2b2";
+        result.style.color = "#737373";
+    }
