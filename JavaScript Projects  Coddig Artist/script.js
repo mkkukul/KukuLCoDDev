5b717c794af72fdd9874c62861efa5227a623ae9
mkkukul <mkkukul@gmail.com>
2024-04-18 20:46:41 +0300

    23

--- JavaScript Projects  Coddig Artist/script.js
+++ JavaScript Projects  Coddig Artist/script.js
@@ -1,6 +1,7 @@
 var myText = document.getElementById("my-text");
 var result = document.getElementById("result");
 var limit = 60;
+
 result.textContent = 0 + "/" + limit;
 
 myText.addEventListener("input",function(){
@@ -11,9 +12,10 @@ myText.addEventListener("input",function(){
         myText.style.borderColor = "#ff2851";
         result.style.color = "#ff2851";
     }
-    
+
     else{
         myText.style.borderColor = "#b2b2b2";
         result.style.color = "#737373";
     }
+    
 });
