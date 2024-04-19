90907e108f840029e6e155e4f223ca792a32542a
mkkukul <mkkukul@gmail.com>
2024-04-19 22:04:40 +0300

    15

--- JavaScript Projects  Coddig Artist/35 Create A Stopwatch Using/script.js
+++ JavaScript Projects  Coddig Artist/35 Create A Stopwatch Using/script.js
@@ -2,3 +2,9 @@ let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
 let timerRef = document.querySelector('.timerDisplay');
 let int = null;
 
+document.getElementById('startTimer').addEventListener('click', ()=>{
+    if(int!==null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer,10);
+});
