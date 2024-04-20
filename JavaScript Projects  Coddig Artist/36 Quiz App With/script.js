43cb7e3b476d8113459fdbf031797cb49bbf9cfc
mkkukul <mkkukul@gmail.com>
2024-04-20 22:18:38 +0300

    25

--- JavaScript Projects  Coddig Artist/36 Quiz App With/script.js
+++ JavaScript Projects  Coddig Artist/36 Quiz App With/script.js
@@ -1,3 +1,4 @@
 let timeLeft = document.querySelector(".time-left");
 let quizContainer = document.getElementById("container");
 let nextBtn = document.getElementById("next-button");
+et countOfQuestion = document.querySelector(".number-of-question");
