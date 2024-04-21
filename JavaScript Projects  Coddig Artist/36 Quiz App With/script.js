852ef7355b77530ba26a810e72eebe34efb55309
mkkukul <mkkukul@gmail.com>
2024-04-21 18:56:07 +0300

    11

--- JavaScript Projects  Coddig Artist/36 Quiz App With/script.js
+++ JavaScript Projects  Coddig Artist/36 Quiz App With/script.js
@@ -91,3 +91,31 @@ const quizDisplay = (questionCount) => {
     //display current question card
     quizCards[questionCount].classList.remove("hide");
 };
+//Quiz Creation
+function quizCreator() {
+    //randomly sort questions
+    quizArray.sort(() => Math.random() - 0.5);
+    //generate quiz
+    for (let i of quizArray) {
+      //randomly sort options
+      i.options.sort(() => Math.random() - 0.5);
+      //quiz card creation
+      let div = document.createElement("div");
+      div.classList.add("container-mid", "hide");
+      //question number
+      countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+      //question
+      let question_DIV = document.createElement("p");
+      question_DIV.classList.add("question");
+      question_DIV.innerHTML = i.question;
+      div.appendChild(question_DIV);
+      //options
+      div.innerHTML += `
+      <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+        <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+         <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+      `;
+      quizContainer.appendChild(div);
+    }
+  }
