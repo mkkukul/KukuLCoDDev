dc8a4353c21f7ff7fa9fd9f2910a4bcb63db496b
mkkukul <mkkukul@gmail.com>
2024-04-22 21:10:49 +0300

    16

---
+++ script.js
@@ -0,0 +1,3 @@
+const container = document.querySelector(".container");
+const addQuestionCard = document.getElementById("add-question-card");
+const cardButton = document.getElementById("save-btn");
