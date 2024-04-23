7a7d7636f85e06a1dc63192e6a0cdf2fd4cae25f
mkkukul <mkkukul@gmail.com>
2024-04-23 21:14:57 +0300

    12

---
+++ JavaScript Projects  Coddig Artist/38/script.js
@@ -0,0 +1,8 @@
+//Initial References
+let timerRef = document.querySelector(".timer-display");
+const hourInput = document.getElementById("hourInput");
+const minuteInput = document.getElementById("minuteInput");
+const activeAlarms = document.querySelector(".activeAlarms");
+const setAlarm = document.getElementById("set");
+let alarmsArray = [];
+let alarmSound = new Audio("./alarm.mp3");
