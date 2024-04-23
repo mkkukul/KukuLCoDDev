3454417ac8059c1f5348c7882eca574c29e517d9
mkkukul <mkkukul@gmail.com>
2024-04-23 21:15:18 +0300

    14

--- JavaScript Projects  Coddig Artist/38/script.js
+++ JavaScript Projects  Coddig Artist/38/script.js
@@ -7,3 +7,23 @@ const setAlarm = document.getElementById("set");
 let alarmsArray = [];
 let alarmSound = new Audio("./alarm.mp3");
 
+let initialHour = 0,
+  initialMinute = 0,
+  alarmIndex = 0;
+//Append zeroes for single digit
+const appendZero = (value) => (value < 10 ? "0" + value : value);
+//Search for value in object
+const searchObject = (parameter, value) => {
+  let alarmObject,
+    objIndex,
+    exists = false;
+  alarmsArray.forEach((alarm, index) => {
+    if (alarm[parameter] == value) {
+      exists = true;
+      alarmObject = alarm;
+      objIndex = index;
+      return false;
+    }
+  });
+  return [exists, alarmObject, objIndex];
+};
