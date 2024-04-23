69c282ade86d9fcd5c144a49ebf8be19492767e3
mkkukul <mkkukul@gmail.com>
2024-04-23 21:17:13 +0300

    156

--- JavaScript Projects  Coddig Artist/38/script.js
+++ JavaScript Projects  Coddig Artist/38/script.js
@@ -47,3 +47,45 @@ function displayTimer() {
       }
     });
 }
+const inputCheck = (inputValue) => {
+    inputValue = parseInt(inputValue);
+    if (inputValue < 10) {
+      inputValue = appendZero(inputValue);
+    }
+    return inputValue;
+  };
+  hourInput.addEventListener("input", () => {
+    hourInput.value = inputCheck(hourInput.value);
+  });
+  minuteInput.addEventListener("input", () => {
+    minuteInput.value = inputCheck(minuteInput.value);
+});
+
+//Create alarm div
+const createAlarm = (alarmObj) => {
+    //Keys from object
+    const { id, alarmHour, alarmMinute } = alarmObj;
+    //Alarm div
+    let alarmDiv = document.createElement("div");
+    alarmDiv.classList.add("alarm");
+    alarmDiv.setAttribute("data-id", id);
+    alarmDiv.innerHTML = `<span>${alarmHour}: ${alarmMinute}</span>`;
+    //checkbox
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.addEventListener("click", (e) => {
+      if (e.target.checked) {
+        startAlarm(e);
+      } else {
+        stopAlarm(e);
+      }
+    });
+    alarmDiv.appendChild(checkbox);
+    //Delete button
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+    deleteButton.classList.add("deleteButton");
+    deleteButton.addEventListener("click", (e) => deleteAlarm(e));
+    alarmDiv.appendChild(deleteButton);
+    activeAlarms.appendChild(alarmDiv);
+};
