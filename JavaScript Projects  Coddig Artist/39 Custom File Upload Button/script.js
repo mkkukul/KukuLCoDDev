a8e221f4a7d22d37a9aa4d393480a0e2691f8562
mkkukul <mkkukul@gmail.com>
2024-04-24 12:45:39 +0300

    15

--- JavaScript Projects  Coddig Artist/39 Custom File Upload Button/script.js
+++ JavaScript Projects  Coddig Artist/39 Custom File Upload Button/script.js
@@ -2,3 +2,6 @@ let fileInput = document.getElementById("file-input");
 let fileList = document.getElementById("files-list");
 let numOfFiles = document.getElementById("num-of-files");
 
+fileInput.addEventListener("change", () => {
+    fileList.innerHTML = "";
+    numOfFiles.textContent = `${fileInput.files.length} Files Selected`;
