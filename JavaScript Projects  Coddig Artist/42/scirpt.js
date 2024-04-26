6ea8f2137e335bee0ea134876566f2e8e3905637
mkkukul <mkkukul@gmail.com>
2024-04-26 12:01:10 +0300

    3

---
+++ JavaScript Projects  Coddig Artist/42/scirpt.js
@@ -0,0 +1,9 @@
+let operators = ["+", "-", "*"];
+const startBtn = document.getElementById("start-btn");
+const question = document.getElementById("question");
+const controls = document.querySelector(".controls-container");
+const result = document.getElementById("result");
+const submitBtn = document.getElementById("submit-btn");
+const errorMessage = document.getElementById("error-msg");
+let answerValue;
+let operatorQuestion;
