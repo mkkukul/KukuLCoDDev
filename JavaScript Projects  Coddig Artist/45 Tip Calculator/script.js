bff43304bb4021fb7c2d28f11a35b3542be55553
mkkukul <mkkukul@gmail.com>
2024-04-28 06:52:20 +0300

    13

--- JavaScript Projects  Coddig Artist/45 Tip Calculator/script.js
+++ JavaScript Projects  Coddig Artist/45 Tip Calculator/script.js
@@ -2,3 +2,4 @@ const sliders = document.querySelectorAll("input[type='range']");
 sliders.forEach(function (slider) {
   slider.addEventListener("input", calculateTip);
 });
+const billInput = document.getElementById("bill");
