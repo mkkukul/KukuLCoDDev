69de19adf1cb9e20782dc1004a62bfcfd242c0fb
mkkukul <mkkukul@gmail.com>
2024-04-29 21:47:28 +0300

    13

--- JavaScript Projects  Coddig Artist/46/script.js
+++ JavaScript Projects  Coddig Artist/46/script.js
@@ -2,3 +2,10 @@ let tabs = document.querySelectorAll(".tabs h3");
 let tabContents = document.querySelectorAll(".tab-content div");
 
 tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabContents.forEach((content) => {
+      content.style.display = "none";
+    });
+    tabContents[index].style.display = "block";
+  });
+});
