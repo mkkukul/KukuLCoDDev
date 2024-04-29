831db36a9739c7b4275e850664e73e922e0fd381
mkkukul <mkkukul@gmail.com>
2024-04-29 22:30:30 +0300

    25

--- JavaScript Projects  Coddig Artist/script.js
+++ JavaScript Projects  Coddig Artist/script.js
@@ -1,9 +1,6 @@
 const result = document.querySelector(".result input");
 const copyBtn = document.getElementById("copy-btn");
-const sliders = document.querySelectorAll(".wrapper input[type='range']");
-const rColor = document.getElementById("red");
-const gColor = document.getElementById("green");
-const bColor = document.getElementById("blue");
+const sliders = document.querySelectorAll(".wrapperinput[type='range']");
 
 let generateColor = () => {
   let rColorValue = rColor.value;
@@ -14,18 +11,3 @@ let generateColor = () => {
   result.value = finalColor;
   document.body.style.backgroundColor = finalColor;
 };
-
-let copyColorCode = () => {
-  result.select();
-  document.execCommand("copy");
-  copyBtn.innerText = "Copied!";
-  setTimeout(() => {
-    copyBtn.innerText = "Copy Color Code";
-  }, 1000);
-};
-
-sliders.forEach((inp) => {
-  inp.addEventListener("input", generateColor);
-});
-window.addEventListener("load", generateColor);
-copyBtn.addEventListener("click", copyColorCode);
