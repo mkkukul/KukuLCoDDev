12b86a8273df988f62122162709ae41dc738e7fc
mkkukul <mkkukul@gmail.com>
2024-04-29 22:28:56 +0300

    23

---
+++ JavaScript Projects  Coddig Artist/script.js
@@ -0,0 +1,3 @@
+const result = document.querySelector(".result input");
+const copyBtn = document.getElementById("copy-btn");
+const sliders = document.querySelectorAll(".wrapperinput[type='range']");
