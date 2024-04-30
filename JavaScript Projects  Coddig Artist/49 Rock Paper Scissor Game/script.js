b7759840d6f05115203f5a0fa4b62354528c23d1
mkkukul <mkkukul@gmail.com>
2024-04-30 12:07:54 +0300

    15

---
+++ JavaScript Projects  Coddig Artist/49 Rock Paper Scissor Game/script.js
@@ -0,0 +1,19 @@
+let [computer_score, user_score] = [0, 0];
+let result_ref = document.getElementById("result");
+let choices_object = {
+  rock: {
+    rock: "draw",
+    scissor: "win",
+    paper: "lose",
+  },
+  scissor: {
+    rock: "lose",
+    scissor: "draw",
+    paper: "win",
+  },
+  paper: {
+    rock: "win",
+    scissor: "lose",
+    paper: "draw",
+  },
+};
