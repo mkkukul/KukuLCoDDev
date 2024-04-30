f8aec9cf5de27dd12c37c08bd300777cc04a825d
mkkukul <mkkukul@gmail.com>
2024-04-30 12:08:15 +0300

    17

--- JavaScript Projects  Coddig Artist/49 Rock Paper Scissor Game/script.js
+++ JavaScript Projects  Coddig Artist/49 Rock Paper Scissor Game/script.js
@@ -21,3 +21,5 @@ let choices_object = {
 function checker(input){
     var choices = ["rock", "paper", "scissor"];
     var num = Math.floor(Math.random()*3);
+    document.getElementById("comp_choice").innerHTML = 
+    ` Computer choose <span> ${choices[num].toUpperCase()} </span>`;
