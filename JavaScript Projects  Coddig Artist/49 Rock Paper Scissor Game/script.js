edb40340425684f89f307e33a29b05814ac5559d
mkkukul <mkkukul@gmail.com>
2024-04-30 12:10:02 +0300

    25

--- JavaScript Projects  Coddig Artist/49 Rock Paper Scissor Game/script.js
+++ JavaScript Projects  Coddig Artist/49 Rock Paper Scissor Game/script.js
@@ -18,26 +18,37 @@ let choices_object = {
   },
 };
 
-function checker(input){
-    var choices = ["rock", "paper", "scissor"];
-    var num = Math.floor(Math.random()*3);
-    document.getElementById("comp_choice").innerHTML = 
-    ` Computer choose <span> ${choices[num].toUpperCase()} </span>`;
-    document.getElementById("user_choice").innerHTML = 
-    ` You choose <span> ${input.toUpperCase()} </span>`;
-    let computer_choice = choices[num];
-    switch(choices_object[input][computer_choice]){
-        case 'win':
-            result_ref.style.cssText = "background-color: #cefdce; color: #689f38";
-            result_ref.innerHTML = "YOU WIN";
-            user_score++;
-            break;
-        case 'lose':
-            result_ref.style.cssText = "background-color: #ffdde0; color: #d32f2f";
-            result_ref.innerHTML = "YOU LOSE";
-            computer_score++;
-            break;
-            default:
-                result_ref.style.cssText = "background-color: #e5e5e5; color: #808080";
-                result_ref.innerHTML = "DRAW";
-                break;
+function checker(input) {
+  var choices = ["rock", "paper", "scissor"];
+  var num = Math.floor(Math.random() * 3);
+
+  document.getElementById(
+    "comp_choice"
+  ).innerHTML = ` Computer choose <span> ${choices[num].toUpperCase()} </span>`;
+
+  document.getElementById(
+    "user_choice"
+  ).innerHTML = ` You choose <span> ${input.toUpperCase()} </span>`;
+
+  let computer_choice = choices[num];
+
+  switch (choices_object[input][computer_choice]) {
+    case "win":
+      result_ref.style.cssText = "background-color: #cefdce; color: #689f38";
+      result_ref.innerHTML = "YOU WIN";
+      user_score++;
+      break;
+    case "lose":
+      result_ref.style.cssText = "background-color: #ffdde0; color: #d32f2f";
+      result_ref.innerHTML = "YOU LOSE";
+      computer_score++;
+      break;
+    default:
+      result_ref.style.cssText = "background-color: #e5e5e5; color: #808080";
+      result_ref.innerHTML = "DRAW";
+      break;
+  }
+
+  document.getElementById("computer_score").innerHTML = computer_score;
+  document.getElementById("user_score").innerHTML = user_score;
+}
