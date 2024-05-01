03e6321c4df5511e9d01e7e1f6f74678b0e8f51a
mkkukul <mkkukul@gmail.com>
2024-05-01 20:56:00 +0300

    8

--- JavaScript Projects  Coddig Artist/50 Captcha Generator/script.js
+++ JavaScript Projects  Coddig Artist/50 Captcha Generator/script.js
@@ -4,3 +4,18 @@ let userInput = document.getElementById("user-input");
 let canvas = document.getElementById("canvas");
 let reloadButton = document.getElementById("reload-button");
 let text = "";
+//Generate Text
+const textGenerator = () => {
+  let generatedText = "";
+  /* String.fromCharCode gives ASCII value from a given number */
+  // total 9 letters hence loop of 3
+  for (let i = 0; i < 3; i++) {
+    //65-90 numbers are capital letters
+    generatedText += String.fromCharCode(randomNumber(65, 90));
+    //97-122 are small letters
+    generatedText += String.fromCharCode(randomNumber(97, 122));
+    //48-57 are numbers from 0-9
+    generatedText += String.fromCharCode(randomNumber(48, 57));
+  }
+  return generatedText;
+};
