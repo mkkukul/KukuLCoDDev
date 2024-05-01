cb1f5458e00dc60e86fb05a327ffd3b21a2e31fb
mkkukul <mkkukul@gmail.com>
2024-05-01 20:55:43 +0300

    7

---
+++ JavaScript Projects  Coddig Artist/50 Captcha Generator/script.js
@@ -0,0 +1,6 @@
+//Initial References
+let submitButton = document.getElementById("submit-button");
+let userInput = document.getElementById("user-input");
+let canvas = document.getElementById("canvas");
+let reloadButton = document.getElementById("reload-button");
+let text = "";
