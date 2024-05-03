099023e1e93eb43ae3ff699e612bdd799603a3e9
mkkukul <mkkukul@gmail.com>
2024-05-03 07:28:53 +0300

    13

---
+++ JavaScript Projects  Coddig Artist/52 Drag And Drop File Upload/script.js
@@ -0,0 +1,6 @@
+let uploadButton = document.getElementById("upload-button");
+let chosenImage = document.getElementById("chosen-image");
+let fileName = document.getElementById("file-name");
+let container = document.querySelector(".container");
+let error = document.getElementById("error");
+let imageDisplay = document.getElementById("image-display");
