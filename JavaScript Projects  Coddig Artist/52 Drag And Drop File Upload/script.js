46c9713b5cf62512f30ddf416e1e024879bc14a4
mkkukul <mkkukul@gmail.com>
2024-05-03 07:30:00 +0300

    18

--- JavaScript Projects  Coddig Artist/52 Drag And Drop File Upload/script.js
+++ JavaScript Projects  Coddig Artist/52 Drag And Drop File Upload/script.js
@@ -5,14 +5,22 @@ let container = document.querySelector(".container");
 let error = document.getElementById("error");
 let imageDisplay = document.getElementById("image-display");
 
-
 const fileHandler = (file, name, type) => {
-    if (type.split("/")[0] !== "image") {
-      //File Type Error
-      error.innerText = "Please upload an image file";
-      return false;
-    }
-    error.innerText = "";
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
+  if (type.split("/")[0] !== "image") {
+    //File Type Error
+    error.innerText = "Please upload an image file";
+    return false;
+  }
+  error.innerText = "";
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = () => {
+    //image and file name
+    let imageContainer = document.createElement("figure");
+    let img = document.createElement("img");
+    img.src = reader.result;
+    imageContainer.appendChild(img);
+    imageContainer.innerHTML += `<figcaption>${name}</figcaption>`;
+    imageDisplay.appendChild(imageContainer);
+  };
+};
