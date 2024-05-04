bb3ec1452941cae0217fc2cfcd565183e2423124
mkkukul <mkkukul@gmail.com>
2024-05-04 21:24:32 +0300

    20

--- JavaScript Projects  Coddig Artist/script.js
+++ JavaScript Projects  Coddig Artist/script.js
@@ -25,10 +25,11 @@ const checkError = (error) => {
   }
 };
 const showLocation = async (position) => {
-    //We user the NOminatim API for getting actual addres from latitude and longitude
-    let response = await fetch(
-        `https://nominatim.openstreetmap.org/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`
-    );
-    //store response object
-    let data = await response.json();
-    
+  //We user the NOminatim API for getting actual addres from latitude and longitude
+  let response = await fetch(
+    `https://nominatim.openstreetmap.org/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`
+  );
+  //store response object
+  let data = await response.json();
+  locationDiv.innerText = `${data.address.city}, ${data.address.country}`;
+};
