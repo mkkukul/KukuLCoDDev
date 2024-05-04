455f68efca86bd27139ea9c13beedc776b118833
mkkukul <mkkukul@gmail.com>
2024-05-04 21:19:17 +0300

    8

--- JavaScript Projects  Coddig Artist/script.js
+++ JavaScript Projects  Coddig Artist/script.js
@@ -2,8 +2,12 @@ let locationButton = document.getElementById("get-location");
 let locationDiv = document.getElementById("location-details");
 
 locationButton.addEventListener("click", () => {
-    //Geolocation APU is used to get geographical position of a user and is available inside the navigator object
-    if (navigator.geolocation) {
-        //returns position(latitude and longitude) or error
+  //Geolocation APU is used to get geographical position of a user and is available inside the navigator object
+  if (navigator.geolocation) {
+    //returns position(latitude and longitude) or error
     navigator.geolocation.getCurrentPosition(showLocation, checkError);
-} else {
+  } else {
+    //For old browser i.e IE
+    locationDiv.innerText = "The browser does not support geolocation";
+  }
+});
