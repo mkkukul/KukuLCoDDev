fd70b8c43c70a41257888f058d49732512c52bf6
mkkukul <mkkukul@gmail.com>
2024-05-04 21:17:58 +0300

    6

--- JavaScript Projects  Coddig Artist/script.js
+++ JavaScript Projects  Coddig Artist/script.js
@@ -1,2 +1,6 @@
 let locationButton = document.getElementById("get-location");
 let locationDiv = document.getElementById("location-details");
+
+locationButton.addEventListener("click", () => {
+    //Geolocation APU is used to get geographical position of a user and is available inside the navigator object
+    if (navigator.geolocation) {
