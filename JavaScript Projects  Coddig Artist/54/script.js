6743b71a1e6527a280b5fc3b953df5c390d78841
mkkukul <mkkukul@gmail.com>
2024-05-05 06:51:56 +0300

    13

--- JavaScript Projects  Coddig Artist/54/script.js
+++ JavaScript Projects  Coddig Artist/54/script.js
@@ -9,3 +9,5 @@ let msg = document.getElementById("msg");
 
 function strengthChecker(){
     let password = document.getElementById("password").value;
+    parameters.letters = (/[A-Za-z]+/.test(password))?true:false;
+    parameters.numbers = (/[0-9]+/.test(password))?
