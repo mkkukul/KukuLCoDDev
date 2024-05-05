c81990ba8c35981a14dba8e9977fb87c718935ac
mkkukul <mkkukul@gmail.com>
2024-05-05 06:55:51 +0300

    24

--- JavaScript Projects  Coddig Artist/54/script.js
+++ JavaScript Projects  Coddig Artist/54/script.js
@@ -47,3 +47,7 @@ function strengthChecker(){
 function toggle(){
     let password = document.getElementById("password");
     let eye = document.getElementById("toggle");
+    if(password.getAttribute("type") == "password"){
+        password.setAttribute("type","text");
+        eye.style.color = "#0be881";
+    }
