fd7787e6f8632bba5e4db62f75c07cf113b0e672
mkkukul <mkkukul@gmail.com>
2024-05-05 06:53:29 +0300

    19

--- JavaScript Projects  Coddig Artist/54/script.js
+++ JavaScript Projects  Coddig Artist/54/script.js
@@ -22,3 +22,7 @@ function strengthChecker(){
         span.classList.add("strength");
         strengthBar.appendChild(span);
     }
+    let spanRef = document.getElementsByClassName("strength");
+    for( let i = 0; i < spanRef.length; i++){
+        switch(spanRef.length - 1){
+            case 0 :
