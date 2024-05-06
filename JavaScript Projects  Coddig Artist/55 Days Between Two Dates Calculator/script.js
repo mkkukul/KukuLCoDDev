fb7072cda126a128f4721ab9318c50a0b70d5f9e
mkkukul <mkkukul@gmail.com>
2024-05-06 09:14:47 +0300

    17

--- JavaScript Projects  Coddig Artist/55 Days Between Two Dates Calculator/script.js
+++ JavaScript Projects  Coddig Artist/55 Days Between Two Dates Calculator/script.js
@@ -14,3 +14,6 @@ submit.addEventListener("click", () => {
     //Calculate difference in time using getTime function
     //getTime calculates number of years since January 1,1970
     let timeDifference = date2.getTime() - date1.getTime();
+    //Since this value is in milliseconds we need to convert it into days
+    //We want the difference to be a non-negative number. Hence we use Math.abs()
+    let dayDifference = Math.abs(timeDifference / (1000 * 3600 * 24))
