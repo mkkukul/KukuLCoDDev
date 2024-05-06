c03e13e47240fe75abb11606d2c8716339a074d8
mkkukul <mkkukul@gmail.com>
2024-05-06 09:15:17 +0300

    20

--- JavaScript Projects  Coddig Artist/55 Days Between Two Dates Calculator/script.js
+++ JavaScript Projects  Coddig Artist/55 Days Between Two Dates Calculator/script.js
@@ -2,9 +2,8 @@
 let submit = document.getElementById("submit");
 let output = document.getElementById("output");
 
-
 submit.addEventListener("click", () => {
-    //Create a Date object from input value
+  //Create a Date object from input value
   let date1 = new Date(document.getElementById("date-1").value);
   let date2 = new Date(document.getElementById("date-2").value);
 
@@ -16,6 +15,11 @@ submit.addEventListener("click", () => {
     let timeDifference = date2.getTime() - date1.getTime();
     //Since this value is in milliseconds we need to convert it into days
     //We want the difference to be a non-negative number. Hence we use Math.abs()
-    let dayDifference = Math.abs(timeDifference / (1000 * 3600 * 24))
+    let dayDifference = Math.abs(timeDifference / (1000 * 3600 * 24));
     output.innerHTML = `Difference between the two dates is <span>${dayDifference}</span> days`;
   }
+  //Else display that the input is valid
+  else {
+    output.innerHTML = "Please select a valid date";
+  }
+});
