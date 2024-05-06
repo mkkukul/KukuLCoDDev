b1bd1fbb0b0638ae95132b2b0eee1c958bbb0968
mkkukul <mkkukul@gmail.com>
2024-05-06 09:14:07 +0300

    14

--- JavaScript Projects  Coddig Artist/55 Days Between Two Dates Calculator/script.js
+++ JavaScript Projects  Coddig Artist/55 Days Between Two Dates Calculator/script.js
@@ -6,3 +6,4 @@ let output = document.getElementById("output");
 submit.addEventListener("click", () => {
     //Create a Date object from input value
   let date1 = new Date(document.getElementById("date-1").value);
+  let date2 = new Date(document.getElementById("date-2").value);
