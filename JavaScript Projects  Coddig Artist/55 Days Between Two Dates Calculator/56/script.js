2727696b3d838a823c83f04772990363013b837d
mkkukul <mkkukul@gmail.com>
2024-05-07 11:01:07 +0300

    14

--- JavaScript Projects  Coddig Artist/55 Days Between Two Dates Calculator/56/script.js
+++ JavaScript Projects  Coddig Artist/55 Days Between Two Dates Calculator/56/script.js
@@ -1,3 +1,5 @@
 let meme = document.getElementById("meme");
 let title = document.getElementById("title");
 let getMemeBtn = document.getElementById("get-meme-btn");
+//API URL
+let url = " https://meme-api.herokuapp.com/gimme/";
