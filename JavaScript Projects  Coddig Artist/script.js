0ed0ca8562714abbc310c6e174039031fce9ed16
mkkukul <mkkukul@gmail.com>
2024-05-08 12:26:27 +0300

    14

--- JavaScript Projects  Coddig Artist/script.js
+++ JavaScript Projects  Coddig Artist/script.js
@@ -3,3 +3,5 @@ const moves = document.getElementById("moves");
 const container = document.querySelector(".container");
 const startButton = document.getElementById("start-button");
 const coverScreen = document.querySelector(".cover-screen");
+const result = document.getElementById("result");
+let currentElement = "";
