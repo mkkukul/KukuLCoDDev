31194c2bc60fdad8a2c5a87e879253d98f809336
mkkukul <mkkukul@gmail.com>
2024-05-08 12:28:53 +0300

    24

--- JavaScript Projects  Coddig Artist/script.js
+++ JavaScript Projects  Coddig Artist/script.js
@@ -80,3 +80,23 @@ const selectImage = (e) => {
     //get row and col values for both elements
   const [row1, col1] = getCoords(currentParent);
   const [row2, col2] = getCoords(targetParent);
+  if (checkAdjacent(row1, row2, col1, col2)) {
+    //Swap
+    currentElement.remove();
+    targetElement.remove();
+    //Get image index(to be used later for manipulating array)
+    let currentIndex = parseInt(currentElement.getAttribute("data-index"));
+    let targetIndex = parseInt(targetElement.getAttribute("data-index"));
+    //Swap Index
+    currentElement.setAttribute("data-index", targetIndex);
+    targetElement.setAttribute("data-index", currentIndex);
+    //Swap Images
+    currentParent.appendChild(targetElement);
+    targetParent.appendChild(currentElement);
+    //Array swaps
+    let currentArrIndex = imagesArr.indexOf(currentIndex);
+    let targetArrIndex = imagesArr.indexOf(targetIndex);
+    [imagesArr[currentArrIndex], imagesArr[targetArrIndex]] = [
+      imagesArr[targetArrIndex],
+      imagesArr[currentArrIndex],
+    ];
