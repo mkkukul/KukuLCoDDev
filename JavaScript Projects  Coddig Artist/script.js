7a44646c781cc78d7ae8e6be0c3000472ffe14da
mkkukul <mkkukul@gmail.com>
2024-05-08 12:26:37 +0300

    15

--- JavaScript Projects  Coddig Artist/script.js
+++ JavaScript Projects  Coddig Artist/script.js
@@ -5,3 +5,14 @@ const startButton = document.getElementById("start-button");
 const coverScreen = document.querySelector(".cover-screen");
 const result = document.getElementById("result");
 let currentElement = "";
+let movesCount,
+  imagesArr = [];
+const isTouchDevice = () => {
+  try {
+    //We try to create TouchEvent (it would fail for desktops ad throw error)
+    document.createEvent("TouchEvent");
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
