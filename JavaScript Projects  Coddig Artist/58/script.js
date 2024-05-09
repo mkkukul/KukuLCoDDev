4c5e6fba8c61f1ea1517464ccd6646f76c671bf7
mkkukul <mkkukul@gmail.com>
2024-05-09 11:36:53 +0300

    14

--- JavaScript Projects  Coddig Artist/58/script.js
+++ JavaScript Projects  Coddig Artist/58/script.js
@@ -5,3 +5,11 @@ let generateGif = () => {
     let loader = document.querySelector(".loader");
     loader.style.display = "block";
     document.querySelector(".wrapper").style.display = "none";
+
+    //Get search value (default => laugh)
+    let q = document.getElementById("search-box").value;
+    //We need 10 gifs to be displayed in result
+    let gifCount = 10;
+    //API URL =
+    let finalURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}&limit=${gifCount}&offset=0&rating=g&lang=en`;
+    document.querySelector(".wrapper").innerHTML = "";
