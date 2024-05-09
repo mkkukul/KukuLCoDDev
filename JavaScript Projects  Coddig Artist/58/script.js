faca96827f4adec50a37c40f5b8496f2b6cf55ec
mkkukul <mkkukul@gmail.com>
2024-05-09 11:35:52 +0300

    12

--- JavaScript Projects  Coddig Artist/58/script.js
+++ JavaScript Projects  Coddig Artist/58/script.js
@@ -1,1 +1,6 @@
 let submitBtn = document.getElementById("submit-btn");
+
+let generateGif = () => {
+    //display loader until gif load
+    let loader = document.querySelector(".loader");
+    loader.style.display = "block";
