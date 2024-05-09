5866dbae76024d2824203ff2c6fdf45bd15870c4
mkkukul <mkkukul@gmail.com>
2024-05-09 11:40:04 +0300

    24

--- JavaScript Projects  Coddig Artist/58/script.js
+++ JavaScript Projects  Coddig Artist/58/script.js
@@ -1,27 +1,27 @@
 let submitBtn = document.getElementById("submit-btn");
 
 let generateGif = () => {
-    //display loader until gif load
-    let loader = document.querySelector(".loader");
-    loader.style.display = "block";
-    document.querySelector(".wrapper").style.display = "none";
+  //display loader until gif load
+  let loader = document.querySelector(".loader");
+  loader.style.display = "block";
+  document.querySelector(".wrapper").style.display = "none";
 
-    //Get search value (default => laugh)
-    let q = document.getElementById("search-box").value;
-    //We need 10 gifs to be displayed in result
-    let gifCount = 10;
-    //API URL =
-    let finalURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}&limit=${gifCount}&offset=0&rating=g&lang=en`;
-    document.querySelector(".wrapper").innerHTML = "";
+  //Get search value (default => laugh)
+  let q = document.getElementById("search-box").value;
+  //We need 10 gifs to be displayed in result
+  let gifCount = 10;
+  //API URL =
+  let finalURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}&limit=${gifCount}&offset=0&rating=g&lang=en`;
+  document.querySelector(".wrapper").innerHTML = "";
 
-    //Make a call to API
-    fetch(finalURL)
+  //Make a call to API
+  fetch(finalURL)
     .then((resp) => resp.json())
     .then((info) => {
-    console.log(info.data);
-    //All gifs
-    let gifsData = info.data;
-    gifsData.forEach((gif) => {
+      console.log(info.data);
+      //All gifs
+      let gifsData = info.data;
+      gifsData.forEach((gif) => {
         //Generate cards for every gif
         let container = document.createElement("div");
         container.classList.add("container");
@@ -42,7 +42,7 @@ let generateGif = () => {
         let copyBtn = document.createElement("button");
         copyBtn.innerText = "Copy Link";
         copyBtn.onclick = () => {
-            //Append the obtained ID to default URL
+          //Append the obtained ID to default URL
           let copyLink = `https://media4.giphy.com/media/${gif.id}/giphy.mp4`;
           //Copy text inside the text field
           navigator.clipboard
@@ -51,16 +51,23 @@ let generateGif = () => {
               alert("GIF copied to clipboard");
             })
             .catch(() => {
-                //if navigator is not supported
-                alert("GIF copied to clipboard");
-                //create temporary input
-                let hiddenInput = document.createElement("input");
-                hiddenInput.setAttribute("type", "text");
-                document.body.appendChild(hiddenInput);
-                hiddenInput.value = copyLink;
-                 //Select input
-                hiddenInput.select();
-                //Copy the value
+              //if navigator is not supported
+              alert("GIF copied to clipboard");
+              //create temporary input
+              let hiddenInput = document.createElement("input");
+              hiddenInput.setAttribute("type", "text");
+              document.body.appendChild(hiddenInput);
+              hiddenInput.value = copyLink;
+              //Select input
+              hiddenInput.select();
+              //Copy the value
               document.execCommand("copy");
               //remove the input
               document.body.removeChild(hiddenInput);
+            });
+        };
+        container.append(copyBtn);
+        document.querySelector(".wrapper").append(container);
+      });
+    });
+};
