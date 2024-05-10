f4bddbd9f73d34abc107029504dd9cc766951303
mkkukul <mkkukul@gmail.com>
2024-05-10 05:40:19 +0300

    14

--- JavaScript Projects  Coddig Artist/59/script.js
+++ JavaScript Projects  Coddig Artist/59/script.js
@@ -2,3 +2,6 @@ let filterA = document.getElementById("blur");
 let filterB = document.getElementById("contrast");
 let filterC = document.getElementById("hue-rotate");
 let filterD = document.getElementById("sepia");
+
+let noFlipBtn = document.getElementById("no-flip");
+let flipXBtn = document.getElementById("flip-x");
