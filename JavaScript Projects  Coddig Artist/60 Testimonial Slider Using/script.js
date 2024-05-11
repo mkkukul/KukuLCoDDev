584650299f167dc2333bbd1ec62d3b6e92cfc6c8
mkkukul <mkkukul@gmail.com>
2024-05-11 07:47:34 +0300

    14

--- JavaScript Projects  Coddig Artist/60 Testimonial Slider Using/script.js
+++ JavaScript Projects  Coddig Artist/60 Testimonial Slider Using/script.js
@@ -8,8 +8,9 @@ const testimonials = [
         "Neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur",
     },
     {
-        name: "Katey Topaz",
-        job: "Developer, TechCrew",
-        {
-            name: "Katey Topaz",
-            job: "Developer, TechCrew",
+      name: "Katey Topaz",
+      job: "Developer, TechCrew",
+      image: "profile-image-2.png",
+      testimonial:
+        "Elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla",
+    },
