bebe04d9a3421416bd2fffb4120e977a587090cf
mkkukul <mkkukul@gmail.com>
2024-05-11 07:46:37 +0300

    11

---
+++ JavaScript Projects  Coddig Artist/60 Testimonial Slider Using/script.js
@@ -0,0 +1,9 @@
+//Testimonial Data
+const testimonials = [
+    {
+      name: "Eva Sawyer",
+      job: "CEO, Fashworks",
+      image: "profile-image-1.png",
+      testimonial:
+        "Neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur",
+    },
