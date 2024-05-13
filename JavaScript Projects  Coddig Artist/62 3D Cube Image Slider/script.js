8fa0a2e2809b8f41bb8c1752144ce523d0a56856
mkkukul <mkkukul@gmail.com>
2024-05-13 09:30:03 +0300

    20

--- JavaScript Projects  Coddig Artist/62 3D Cube Image Slider/script.js
+++ JavaScript Projects  Coddig Artist/62 3D Cube Image Slider/script.js
@@ -3,6 +3,7 @@ let btnNext = document.getElementById("next");
 let btnPrev = document.getElementById("prev");
 
 let pos = 0;
+
 btnNext.addEventListener("click", () => {
   pos -= 90;
   cube.style.transform = `rotateY(${pos}deg)`;
