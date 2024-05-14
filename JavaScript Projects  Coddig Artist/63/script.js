1f6428e65c452b5dd17a5fd90ae2e7b17bb3c6f5
mkkukul <mkkukul@gmail.com>
2024-05-14 15:33:58 +0300

    12

--- JavaScript Projects  Coddig Artist/63/script.js
+++ JavaScript Projects  Coddig Artist/63/script.js
@@ -1,2 +1,6 @@
 let canvas = document.getElementById("scratch");
 let context = canvas.getContext("2d");
+
+const init = () => {
+    let gradientColor = context.createLinearGradient(0, 0, 135, 135);
+    gradientColor.addColorStop(0, "#c3a3f1");
