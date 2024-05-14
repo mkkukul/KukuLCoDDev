0a9ba988b110f1c826b0da4e127256b3efeb8bdf
mkkukul <mkkukul@gmail.com>
2024-05-14 15:34:15 +0300

    14

--- JavaScript Projects  Coddig Artist/63/script.js
+++ JavaScript Projects  Coddig Artist/63/script.js
@@ -8,3 +8,7 @@ const init = () => {
   context.fillStyle = gradientColor;
   context.fillRect(0, 0, 200, 200);
 };
+//initially mouse X and mouse Y positions are 0
+let mouseX = 0;
+let mouseY = 0;
+let isDragged = false;
