09de486c0613765d01dc04f89bb3df2cafccc8a7
mkkukul <mkkukul@gmail.com>
2024-05-15 12:45:27 +0300

    25

--- JavaScript Projects  Coddig Artist/64 Minion Eyes Follow Mouse Cursor/script.js
+++ JavaScript Projects  Coddig Artist/64 Minion Eyes Follow Mouse Cursor/script.js
@@ -3,6 +3,7 @@ let eye_ref = document.querySelectorAll(".eye");
 
 //mousemove for devices with mouse aand touchmove for touchcreen devices
 let events = ["mousemove", "touchmove"];
+
 //Check for touch screen
 function isTouchDevice() {
   try {
@@ -12,6 +13,7 @@ function isTouchDevice() {
     return false;
   }
 }
+
 //Same function for both events
 events.forEach((eventType) => {
   document.body.addEventListener(eventType, (event) => {
@@ -19,13 +21,16 @@ events.forEach((eventType) => {
       /* getBoundingClientRect() method returns the position relative to the viewport */
       let eyeX = eye.getBoundingClientRect().left + eye.clientWidth / 2;
       let eyeY = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+
       /* ClientX and ClientY return the position of clients cursor from top left of the screen*/
       var x = !isTouchDevice() ? event.clientX : event.touches[0].clientX;
       var y = !isTouchDevice() ? event.clientY : event.touches[0].clientY;
+
       /* 
-        Subtract x position of mouse from x position of eye and y position of mouse from y position of eye.
-        Use atan2(returns angle in radians)
-        */
+    Subtract x position of mouse from x position of eye and y position of mouse from y position of eye.
+    Use atan2(returns angle in radians)
+    */
+
       let radian = Math.atan2(x - eyeX, y - eyeY);
       //Convert Radians to Degrees
       let rotationDegrees = radian * (180 / Math.PI) * -1 + 180;
