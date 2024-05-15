68852a7c88edb1fd794907a7977c7c02ad855dc2
mkkukul <mkkukul@gmail.com>
2024-05-15 12:42:32 +0300

    13

--- JavaScript Projects  Coddig Artist/64 Minion Eyes Follow Mouse Cursor/script.js
+++ JavaScript Projects  Coddig Artist/64 Minion Eyes Follow Mouse Cursor/script.js
@@ -1,2 +1,5 @@
 //Selecting the eye div
 let eye_ref = document.querySelectorAll(".eye");
+
+//mousemove for devices with mouse aand touchmove for touchcreen devices
+let events = ["mousemove", "touchmove"];
