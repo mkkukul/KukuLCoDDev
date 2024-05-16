7beae1c817bdebd484df509ad400ef64c93329e9
mkkukul <mkkukul@gmail.com>
2024-05-16 08:38:46 +0300

    18

--- JavaScript Projects  Coddig Artist/65 Simple Interest Calculator/script.js
+++ JavaScript Projects  Coddig Artist/65 Simple Interest Calculator/script.js
@@ -3,3 +3,4 @@ let result = document.getElementById("result");
 let calculate = () => {
     let p = Number(document.getElementById("principal").value);
     let r = Number(document.getElementById("rate").value);
+    let t = Number(document.getElementById("time").value);
