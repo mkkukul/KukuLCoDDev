4721b7341466207c0a9b3a5311ca88e7d7ad7d0c
mkkukul <mkkukul@gmail.com>
2024-05-17 06:43:30 +0300

    11

---
+++ JavaScript Projects  Coddig Artist/66 Form Validation Using/script.js
@@ -0,0 +1,4 @@
+//First Name
+let firstNameInput = document.getElementById("first-name-input");
+let firstNameError = document.getElementById("first-name-error");
+let emptyFirstNameError = document.getElementById("empty-first-name");
