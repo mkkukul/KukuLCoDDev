fc9a516d69a6719184e18cb37b0d3c8a1e56018a
mkkukul <mkkukul@gmail.com>
2024-05-18 09:24:51 +0300

    13

--- JavaScript Projects  Coddig Artist/67 Pomodoro Timer/script.js
+++ JavaScript Projects  Coddig Artist/67 Pomodoro Timer/script.js
@@ -1,3 +1,7 @@
 let focusButton = document.getElementById("focus");
 let buttons = document.querySelectorAll(".btn");
 let shortBreakButton = document.getElementById("shortbreak");
+let longBreakButton = document.getElementById("longbreak");
+let startBtn = document.getElementById("btn-start");
+let reset = document.getElementById("btn-reset");
+let pause = document.getElementById("btn-pause");
