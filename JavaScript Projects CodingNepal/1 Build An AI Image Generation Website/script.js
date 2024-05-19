cc676f29eb67f305dea24d71e50604b9b71877d4
mkkukul <mkkukul@gmail.com>
2024-05-19 18:33:03 +0300

    27

--- JavaScript Projects CodingNepal/1 Build An AI Image Generation Website/script.js
+++ JavaScript Projects CodingNepal/1 Build An AI Image Generation Website/script.js
@@ -20,4 +20,32 @@ const updateImageCard = (imgDataArray) => {
       }
     });
   }
-  
+
+  const generateAiImages = async (userPrompt, userImgQuantity) => {
+    try {
+      // Send a request to the OpenAI API to generate images based on user inputs
+      const response = await fetch("https://api.openai.com/v1/images/generations", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          prompt: userPrompt,
+          n: userImgQuantity,
+          size: "512x512",
+          response_format: "b64_json"
+        }),
+      });
+      // Throw an error message if the API response is unsuccessful
+      if(!response.ok) throw new Error("Failed to generate AI images. Make sure your API key is valid.");
+      const { data } = await response.json(); // Get data from the response
+      updateImageCard([...data]);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      generateBtn.removeAttribute("disabled");
+      generateBtn.innerText = "Generate";
+      isImageGenerating = false;
+    }
+  }
