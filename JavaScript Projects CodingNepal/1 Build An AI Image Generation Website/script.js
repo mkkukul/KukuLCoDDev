57ff777e7498990f3bc2034d47e01a13451ee554
mkkukul <mkkukul@gmail.com>
2024-05-19 18:31:49 +0300

    22

--- JavaScript Projects CodingNepal/1 Build An AI Image Generation Website/script.js
+++ JavaScript Projects CodingNepal/1 Build An AI Image Generation Website/script.js
@@ -1,3 +1,6 @@
 const generateForm = document.querySelector(".generate-form");
 const generateBtn = generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector(".image-gallery");
+
+onst OPENAI_API_KEY = "YOUR-OPENAI-API-KEY-HERE"; // Your OpenAI API key here
+let isImageGenerating = false;
