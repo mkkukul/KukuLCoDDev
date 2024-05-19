19598fc422f8ec74ba6e265169edf266ac7c1c84
mkkukul <mkkukul@gmail.com>
2024-05-19 18:32:44 +0300

    26

--- JavaScript Projects CodingNepal/1 Build An AI Image Generation Website/script.js
+++ JavaScript Projects CodingNepal/1 Build An AI Image Generation Website/script.js
@@ -12,3 +12,12 @@ const updateImageCard = (imgDataArray) => {
     // Set the image source to the AI-generated image data
     const aiGeneratedImage = `data:image/jpeg;base64,${imgObject.b64_json}`;
     imgElement.src = aiGeneratedImage;
+    // When the image is loaded, remove the loading class and set download attributes
+    imgElement.onload = () => {
+        imgCard.classList.remove("loading");
+        downloadBtn.setAttribute("href", aiGeneratedImage);
+        downloadBtn.setAttribute("download", `${new Date().getTime()}.jpg`);
+      }
+    });
+  }
+  
