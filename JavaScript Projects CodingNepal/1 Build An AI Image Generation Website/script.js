d994b3cc5afb98684edee7b72237eac41bf6f1c8
mkkukul <mkkukul@gmail.com>
2024-05-19 18:31:40 +0300

    21

---
+++ JavaScript Projects CodingNepal/1 Build An AI Image Generation Website/script.js
@@ -0,0 +1,3 @@
+const generateForm = document.querySelector(".generate-form");
+const generateBtn = generateForm.querySelector(".generate-btn");
+const imageGallery = document.querySelector(".image-gallery");
