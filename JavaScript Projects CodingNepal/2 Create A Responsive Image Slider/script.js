042a52e47a8f1ef667e4b7e9a8f1b57992d46425
mkkukul <mkkukul@gmail.com>
2024-05-20 07:39:48 +0300

    18

--- JavaScript Projects CodingNepal/2 Create A Responsive Image Slider/script.js
+++ JavaScript Projects CodingNepal/2 Create A Responsive Image Slider/script.js
@@ -1,1 +1,3 @@
 const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
