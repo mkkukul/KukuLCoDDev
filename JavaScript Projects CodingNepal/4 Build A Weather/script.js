662be050b4ecc4cd5c7ffe66ab994f69e059d96d
mkkukul <mkkukul@gmail.com>
2024-05-22 06:06:52 +0300

    24

--- JavaScript Projects CodingNepal/4 Build A Weather/script.js
+++ JavaScript Projects CodingNepal/4 Build A Weather/script.js
@@ -99,6 +99,8 @@ const getUserCoordinates = () => {
             }
         });
 }
-
+locationButton.addEventListener("click", getUserCoordinates);
+searchButton.addEventListener("click", getCityCoordinates);
+cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
 
 
