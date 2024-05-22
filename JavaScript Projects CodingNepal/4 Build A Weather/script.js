59c5bc38e32875cec87435e42c20bbe619b366d2
mkkukul <mkkukul@gmail.com>
2024-05-22 06:04:51 +0300

    16

--- JavaScript Projects CodingNepal/4 Build A Weather/script.js
+++ JavaScript Projects CodingNepal/4 Build A Weather/script.js
@@ -5,14 +5,34 @@ const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 const API_KEY = "YOUR-API-KEY-HERE"; // API key for OpenWeatherMap API
 const createWeatherCard = (cityName, weatherItem, index) => {
-    if(index === 0) { // HTML for the main weather card
-        return `<div class="details">
+  if (index === 0) {
+    // HTML for the main weather card
+    return `<div class="details">
                     <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(
+                      2
+                    )}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </div>
                 <div class="icon">
-                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <img src="https://openweathermap.org/img/wn/${
+                      weatherItem.weather[0].icon
+                    }@4x.png" alt="weather-icon">
                     <h6>${weatherItem.weather[0].description}</h6>
                 </div>`;
+  } else {
+    // HTML for the other five day forecast card
+    return `<li class="card">
+                            <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                            <img src="https://openweathermap.org/img/wn/${
+                              weatherItem.weather[0].icon
+                            }@4x.png" alt="weather-icon">
+                            <h6>Temp: ${(
+                              weatherItem.main.temp - 273.15
+                            ).toFixed(2)}°C</h6>
+                            <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                            <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                        </li>`;
+  }
+};
