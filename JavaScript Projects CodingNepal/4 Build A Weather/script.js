78f180ebb0dffc4a669d0e440bd360fd6c9d8fa2
mkkukul <mkkukul@gmail.com>
2024-05-22 06:05:31 +0300

    19

--- JavaScript Projects CodingNepal/4 Build A Weather/script.js
+++ JavaScript Projects CodingNepal/4 Build A Weather/script.js
@@ -41,3 +41,10 @@ const getWeatherDetails = (cityName, latitude, longitude) => {
     fetch(WEATHER_API_URL).then(response => response.json()).then(data => {
         // Filter the forecasts to get only one forecast per day
         const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!uniqueForecastDays.includes(forecastDate)) {
+                return uniqueForecastDays.push(forecastDate);
+            }
+        });
+
