9127beb68456d2809b23f3b137965212d394fd2b
mkkukul <mkkukul@gmail.com>
2024-05-22 06:03:42 +0300

    14

--- JavaScript Projects CodingNepal/4 Build A Weather/script.js
+++ JavaScript Projects CodingNepal/4 Build A Weather/script.js
@@ -1,3 +1,5 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 const locationButton = document.querySelector(".location-btn");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const weatherCardsDiv = document.querySelector(".weather-cards");
