c6b0375ca3a558f5724ae1038766126e37ae625a
mkkukul <mkkukul@gmail.com>
2024-05-22 06:06:08 +0300

    22

--- JavaScript Projects CodingNepal/4 Build A Weather/script.js
+++ JavaScript Projects CodingNepal/4 Build A Weather/script.js
@@ -64,6 +64,20 @@ const getWeatherDetails = (cityName, latitude, longitude) => {
         alert("An error occurred while fetching the weather forecast!");
     });
 }
+const getCityCoordinates = () => {
+    const cityName = cityInput.value.trim();
+    if (cityName === "") return;
+    const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    
+    // Get entered city coordinates (latitude, longitude, and name) from the API response
+    fetch(API_URL).then(response => response.json()).then(data => {
+        if (!data.length) return alert(`No coordinates found for ${cityName}`);
+        const { lat, lon, name } = data[0];
+        getWeatherDetails(name, lat, lon);
+    }).catch(() => {
+        alert("An error occurred while fetching the coordinates!");
+    });
+}
 
 
 
