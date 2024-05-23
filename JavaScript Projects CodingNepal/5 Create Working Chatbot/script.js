5f98bca308077dd0b771ca1bb504e48b016d04d1
mkkukul <mkkukul@gmail.com>
2024-05-23 21:09:29 +0300

    25

--- JavaScript Projects CodingNepal/5 Create Working Chatbot/script.js
+++ JavaScript Projects CodingNepal/5 Create Working Chatbot/script.js
@@ -80,10 +80,12 @@ chatInput.addEventListener("keydown", (e) => {
     handleChat();
   }
 });
+
 sendChatBtn.addEventListener("click", handleChat);
 closeBtn.addEventListener("click", () =>
   document.body.classList.remove("show-chatbot")
 );
+
 chatbotToggler.addEventListener("click", () =>
   document.body.classList.toggle("show-chatbot")
 );
