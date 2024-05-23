93190df7b89e8d0e58e8245fc241538409e73e88
mkkukul <mkkukul@gmail.com>
2024-05-23 21:07:10 +0300

    20

--- JavaScript Projects CodingNepal/5 Create Working Chatbot/script.js
+++ JavaScript Projects CodingNepal/5 Create Working Chatbot/script.js
@@ -13,3 +13,21 @@ const createChatLi = (message, className) => {
     chatLi.classList.add("chat", `${className}`);
     let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
     chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi; // return chat <li> element
+}
+const generateResponse = (chatElement) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const messageElement = chatElement.querySelector("p");
+    // Define the properties and message for the API request
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: userMessage}],
+        })
+    }
