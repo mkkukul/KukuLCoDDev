643c57e33e8ebdf10e334593e8c32a691b370e9b
mkkukul <mkkukul@gmail.com>
2024-05-23 21:07:54 +0300

    21

--- JavaScript Projects CodingNepal/5 Create Working Chatbot/script.js
+++ JavaScript Projects CodingNepal/5 Create Working Chatbot/script.js
@@ -47,3 +47,21 @@ const generateResponse = (chatElement) => {
     })
     .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
 };
+const handleChat = () => {
+  userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+  if (!userMessage) return;
+  // Clear the input textarea and set its height to default
+  chatInput.value = "";
+  chatInput.style.height = `${inputInitHeight}px`;
+  // Append the user's message to the chatbox
+  chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+
+  setTimeout(() => {
+    // Display "Thinking..." message while waiting for the response
+    const incomingChatLi = createChatLi("Thinking...", "incoming");
+    chatbox.appendChild(incomingChatLi);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    generateResponse(incomingChatLi);
+  }, 600);
+};
