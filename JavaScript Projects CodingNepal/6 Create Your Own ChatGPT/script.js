0f5fd3c308f5cbf6befc60e647cec0509b84faab
mkkukul <mkkukul@gmail.com>
2024-05-24 08:14:35 +0300

    18

--- JavaScript Projects CodingNepal/6 Create Your Own ChatGPT/script.js
+++ JavaScript Projects CodingNepal/6 Create Your Own ChatGPT/script.js
@@ -7,3 +7,7 @@ const deleteButton = document.querySelector("#delete-btn");
 let userText = null;
 const API_KEY = "PASTE-YOUR-API-KEY-HERE"; // Paste your API key here
 const loadDataFromLocalstorage = () => {
+    // Load saved chats and theme from local storage and apply/add on the page
+    const themeColor = localStorage.getItem("themeColor");
+    document.body.classList.toggle("light-mode", themeColor === "light_mode");
+    themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
