58d5699cd45825b671fa8af5d9f8f95515c7d8fc
mkkukul <mkkukul@gmail.com>
2024-05-24 08:14:22 +0300

    17

--- JavaScript Projects CodingNepal/6 Create Your Own ChatGPT/script.js
+++ JavaScript Projects CodingNepal/6 Create Your Own ChatGPT/script.js
@@ -3,3 +3,7 @@ const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
 const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
+
+let userText = null;
+const API_KEY = "PASTE-YOUR-API-KEY-HERE"; // Paste your API key here
+const loadDataFromLocalstorage = () => {
