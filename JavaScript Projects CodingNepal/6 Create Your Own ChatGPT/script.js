2fe2719d80c935f8ff76517e3e825416b4bfbead
mkkukul <mkkukul@gmail.com>
2024-05-24 08:14:10 +0300

    16

---
+++ JavaScript Projects CodingNepal/6 Create Your Own ChatGPT/script.js
@@ -0,0 +1,5 @@
+const chatInput = document.querySelector("#chat-input");
+const sendButton = document.querySelector("#send-btn");
+const chatContainer = document.querySelector(".chat-container");
+const themeButton = document.querySelector("#theme-btn");
+const deleteButton = document.querySelector("#delete-btn");
