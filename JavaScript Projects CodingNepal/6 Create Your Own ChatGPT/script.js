f0b43977e15fd475bb42e9e1ec5feb94fd15702b
mkkukul <mkkukul@gmail.com>
2024-05-24 08:15:03 +0300

    19

--- JavaScript Projects CodingNepal/6 Create Your Own ChatGPT/script.js
+++ JavaScript Projects CodingNepal/6 Create Your Own ChatGPT/script.js
@@ -7,7 +7,17 @@ const deleteButton = document.querySelector("#delete-btn");
 let userText = null;
 const API_KEY = "PASTE-YOUR-API-KEY-HERE"; // Paste your API key here
 const loadDataFromLocalstorage = () => {
-    // Load saved chats and theme from local storage and apply/add on the page
-    const themeColor = localStorage.getItem("themeColor");
-    document.body.classList.toggle("light-mode", themeColor === "light_mode");
-    themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
+  // Load saved chats and theme from local storage and apply/add on the page
+  const themeColor = localStorage.getItem("themeColor");
+  document.body.classList.toggle("light-mode", themeColor === "light_mode");
+  themeButton.innerText = document.body.classList.contains("light-mode")
+    ? "dark_mode"
+    : "light_mode";
+  const defaultText = `<div class="default-text">
+                            <h1>ChatGPT Clone</h1>
+                            <p>Start a conversation and explore the power of AI.<br> Your chat history will be displayed here.</p>
+                        </div>`;
+
+  chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
+  chatContainer.scrollTo(0, chatContainer.scrollHeight); // Scroll to bottom of the chat container
+};
