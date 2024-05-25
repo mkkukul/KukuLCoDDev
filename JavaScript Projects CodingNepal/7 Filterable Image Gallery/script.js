496a40cc1d9c3ad694d75cc1aca52ceec8ff96b5
mkkukul <mkkukul@gmail.com>
2024-05-25 12:08:44 +0300

    12

---
+++ JavaScript Projects CodingNepal/7 Filterable Image Gallery/script.js
@@ -0,0 +1,3 @@
+// Select relevant HTML elements
+const filterButtons = document.querySelectorAll("#filter-buttons button");
+const filterableCards = document.querySelectorAll("#filterable-cards .card");
