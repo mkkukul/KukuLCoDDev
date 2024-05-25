d9bfcd722edd8d9fffa304cf4b2c8d670670220f
mkkukul <mkkukul@gmail.com>
2024-05-25 12:12:55 +0300

    26

---
+++ JavaScript Projects CodingNepal/8 Create A Draggable Bottom Sheet Modal/script.js
@@ -0,0 +1,6 @@
+// Select DOM elements
+const showModalBtn = document.querySelector(".show-modal");
+const bottomSheet = document.querySelector(".bottom-sheet");
+const sheetOverlay = bottomSheet.querySelector(".sheet-overlay");
+const sheetContent = bottomSheet.querySelector(".content");
+const dragIcon = bottomSheet.querySelector(".drag-icon");
