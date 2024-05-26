4cd9f494a4a7ab944ce38d56c4e22db0e7bf4961
mkkukul <mkkukul@gmail.com>
2024-05-26 15:05:35 +0300

    14

---
+++ JavaScript Projects CodingNepal/9/script.js
@@ -0,0 +1,5 @@
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel.children];
