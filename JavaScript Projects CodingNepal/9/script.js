0c77619cea136496d2fb67d2313f6de69d18ce29
mkkukul <mkkukul@gmail.com>
2024-05-26 15:05:47 +0300

    15

--- JavaScript Projects CodingNepal/9/script.js
+++ JavaScript Projects CodingNepal/9/script.js
@@ -3,3 +3,11 @@ const carousel = document.querySelector(".carousel");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const arrowBtns = document.querySelectorAll(".wrapper i");
 const carouselChildrens = [...carousel.children];
+
+let isDragging = false,
+  isAutoPlay = true,
+  startX,
+  startScrollLeft,
+  timeoutId;
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
