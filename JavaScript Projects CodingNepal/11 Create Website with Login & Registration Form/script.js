8488a6cc7616e29d91e96046c53e849b7b4c0569
mkkukul <mkkukul@gmail.com>
2024-05-28 11:54:37 +0300

    22

--- JavaScript Projects CodingNepal/11 Create Website with Login & Registration Form/script.js
+++ JavaScript Projects CodingNepal/11 Create Website with Login & Registration Form/script.js
@@ -9,9 +9,14 @@ const formOpenBtn = document.querySelector("#form-open"),
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
 pwShowHide.forEach((icon) => {
-    icon.addEventListener("click", () => {
-        let getPwInput = icon.parentElement.querySelector("input");
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
     if (getPwInput.type === "password") {
-        getPwInput.type = "text";
+      getPwInput.type = "text";
       icon.classList.replace("uil-eye-slash", "uil-eye");
     } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
