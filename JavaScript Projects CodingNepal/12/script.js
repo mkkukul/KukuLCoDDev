409f4da50cdb167a7e140102a21158d89fa73f13
mkkukul <mkkukul@gmail.com>
2024-05-29 13:16:42 +0300

    13

--- JavaScript Projects CodingNepal/12/script.js
+++ JavaScript Projects CodingNepal/12/script.js
@@ -2,3 +2,7 @@
 const element = document.querySelector(".pagination ul");
 let totalPages = 20;
 let page = 10;
+
+//calling function with passing parameters and adding inside element which is ul tag
+element.innerHTML = createPagination(totalPages, page);
+function createPagination(totalPages, page){
