84fee9919ea283edf5cf5a4a7b701e78f1dd1b2d
mkkukul <mkkukul@gmail.com>
2024-05-29 13:16:53 +0300

    14

--- JavaScript Projects CodingNepal/12/script.js
+++ JavaScript Projects CodingNepal/12/script.js
@@ -6,3 +6,7 @@ let page = 10;
 //calling function with passing parameters and adding inside element which is ul tag
 element.innerHTML = createPagination(totalPages, page);
 function createPagination(totalPages, page){
+    let liTag = '';
+  let active;
+  let beforePage = page - 1;
+  let afterPage = page + 1;
