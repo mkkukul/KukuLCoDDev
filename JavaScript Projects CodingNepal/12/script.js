ae01402e6b6cec9dd0ab4f0755ce6b78d3a68fdf
mkkukul <mkkukul@gmail.com>
2024-05-29 13:17:26 +0300

    15

--- JavaScript Projects CodingNepal/12/script.js
+++ JavaScript Projects CodingNepal/12/script.js
@@ -10,3 +10,6 @@ function createPagination(totalPages, page){
   let active;
   let beforePage = page - 1;
   let afterPage = page + 1;
+  if(page > 1){ //show the next button if the page value is greater than 1
+    liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
+}
