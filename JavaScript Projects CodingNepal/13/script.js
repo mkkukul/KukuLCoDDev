51536146815cd9380bb3a6e6e4280bbfac88a3a1
mkkukul <mkkukul@gmail.com>
2024-05-30 12:24:58 +0300

    15

--- JavaScript Projects CodingNepal/13/script.js
+++ JavaScript Projects CodingNepal/13/script.js
@@ -11,7 +11,13 @@ const getRandomColor = () => {
   return `#${randomHex}`;
 };
 const generateGradient = (isRandom) => {
-    if(isRandom) { // If isRandom is true, update the colors inputs value with random color
-        colorInputs[0].value = getRandomColor();
-        colorInputs[1].value = getRandomColor();
-    }
+  if (isRandom) {
+    // If isRandom is true, update the colors inputs value with random color
+    colorInputs[0].value = getRandomColor();
+    colorInputs[1].value = getRandomColor();
+  }
+  // Creating a gradient string using the select menu value with color input values
+  const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
+  gradientBox.style.background = gradient;
+  textarea.value = `background: ${gradient};`;
+};
