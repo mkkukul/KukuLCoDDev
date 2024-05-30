cd4c439f1fdcf9fb56ef41c6056677ec06887c35
mkkukul <mkkukul@gmail.com>
2024-05-30 12:24:21 +0300

    12

--- JavaScript Projects CodingNepal/13/script.js
+++ JavaScript Projects CodingNepal/13/script.js
@@ -1,3 +1,6 @@
 const gradientBox = document.querySelector(".gradient-box");
 const selectMenu = document.querySelector(".select-box select");
 const colorInputs = document.querySelectorAll(".colors input");
+const textarea = document.querySelector("textarea");
+const refreshBtn = document.querySelector(".refresh");
+const copyBtn = document.querySelector(".copy");
