83431b64ae09eb30698fb8c172f6e3994caf1c78
mkkukul <mkkukul@gmail.com>
2024-05-31 10:41:29 +0300

    26

--- JavaScript Projects CodingNepal/15/script.js
+++ JavaScript Projects CodingNepal/15/script.js
@@ -4,12 +4,14 @@ const body = document.querySelector("body"),
   minuteHand = document.querySelector(".minute"),
   secondHand = document.querySelector(".second"),
   modeSwitch = document.querySelector(".mode-switch");
+
 // check if the mode is already set to "Dark Mode" in localStorage
 if (localStorage.getItem("mode") === "Dark Mode") {
   // add "dark" class to body and set modeSwitch text to "Light Mode"
   body.classList.add("dark");
   modeSwitch.textContent = "Light Mode";
 }
+
 // add a click event listener to modeSwitch
 modeSwitch.addEventListener("click", () => {
   // toggle the "dark" class on the body element
@@ -21,21 +23,22 @@ modeSwitch.addEventListener("click", () => {
   // set localStorage "mode" item based on "dark" class presence
   localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
 });
+
 const updateTime = () => {
   // Get current time and calculate degrees for clock hands
   let date = new Date(),
     secToDeg = (date.getSeconds() / 60) * 360,
     minToDeg = (date.getMinutes() / 60) * 360,
     hrToDeg = (date.getHours() / 12) * 360;
-  (minToDeg = (date.getMinutes() / 60) * 360),
-    (hrToDeg = (date.getHours() / 12) * 360);
 
   // Rotate the clock hands to the appropriate degree based on the current time
   secondHand.style.transform = `rotate(${secToDeg}deg)`;
   minuteHand.style.transform = `rotate(${minToDeg}deg)`;
   hourHand.style.transform = `rotate(${hrToDeg}deg)`;
 };
+
 // call updateTime to set clock hands every second
 setInterval(updateTime, 1000);
+
 //call updateTime function on page load
 updateTime();
