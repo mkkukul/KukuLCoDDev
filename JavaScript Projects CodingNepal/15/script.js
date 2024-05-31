87159ccb05096630b24ecd9bd7057c8d60bdc6fa
mkkukul <mkkukul@gmail.com>
2024-05-31 10:37:48 +0300

    14

--- JavaScript Projects CodingNepal/15/script.js
+++ JavaScript Projects CodingNepal/15/script.js
@@ -1,3 +1,6 @@
 // Get references to DOM elements
 const body = document.querySelector("body"),
   hourHand = document.querySelector(".hour"),
+  minuteHand = document.querySelector(".minute"),
+  secondHand = document.querySelector(".second"),
+  modeSwitch = document.querySelector(".mode-switch");
