baf76f957b8d14c2d8370fb5e32e2cdcc1aad73c
mkkukul <mkkukul@gmail.com>
2024-06-01 20:59:08 +0300

    13

--- JavaScript Projects CodingNepal/16 Drag and Drop Sortable List using/script.js
+++ JavaScript Projects CodingNepal/16 Drag and Drop Sortable List using/script.js
@@ -1,7 +1,10 @@
 const sortableList = document.querySelector(".sortable-list");
 const items = sortableList.querySelectorAll(".item");
-items.forEach(item => {
-    item.addEventListener("dragstart", () => {
-        // Adding dragging class to item after a delay
-        setTimeout(() => item.classList.add("dragging"), 0);
-    });
+items.forEach((item) => {
+  item.addEventListener("dragstart", () => {
+    // Adding dragging class to item after a delay
+    setTimeout(() => item.classList.add("dragging"), 0);
+  });
+  // Removing dragging class from item on dragend event
+  item.addEventListener("dragend", () => item.classList.remove("dragging"));
+});
