63bda50a41ad19c4e48b08e42a49f317381ee408
mkkukul <mkkukul@gmail.com>
2024-06-01 21:00:17 +0300

    17

--- JavaScript Projects CodingNepal/16 Drag and Drop Sortable List using/script.js
+++ JavaScript Projects CodingNepal/16 Drag and Drop Sortable List using/script.js
@@ -9,11 +9,14 @@ items.forEach((item) => {
   item.addEventListener("dragend", () => item.classList.remove("dragging"));
 });
 const initSortableList = (e) => {
-    e.preventDefault();
-    const draggingItem = document.querySelector(".dragging");
-    // Getting all items except currently dragging and making array of them
-    let siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
-    // Finding the sibling after which the dragging item should be placed
-    let nextSibling = siblings.find(sibling => {
-        return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
-
+  e.preventDefault();
+  const draggingItem = document.querySelector(".dragging");
+  // Getting all items except currently dragging and making array of them
+  let siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
+  // Finding the sibling after which the dragging item should be placed
+  let nextSibling = siblings.find((sibling) => {
+    return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+  });
+  // Inserting the dragging item before the found sibling
+  sortableList.insertBefore(draggingItem, nextSibling);
+};
