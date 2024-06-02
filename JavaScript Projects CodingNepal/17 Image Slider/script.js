16693a1d0031c4608536590faabc8b83be7fc920
mkkukul <mkkukul@gmail.com>
2024-06-02 10:04:23 +0300

    16

--- JavaScript Projects CodingNepal/17 Image Slider/script.js
+++ JavaScript Projects CodingNepal/17 Image Slider/script.js
@@ -5,3 +5,9 @@ const wrapper = document.querySelector(".wrapper"),
   buttons = document.querySelectorAll(".button");
   let imageIndex = 1,
   intervalId;
+
+// Define function to start automatic image slider
+const autoSlide = () => {
+    // Start the slideshow by calling slideImage() every 2 seconds
+    intervalId = setInterval(() => slideImage(++imageIndex), 2000);
+  };
