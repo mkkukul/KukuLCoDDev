f713225a7410fb36294ae812b56f7efbc49820ca
mkkukul <mkkukul@gmail.com>
2024-06-02 10:04:06 +0300

    15

--- JavaScript Projects CodingNepal/17 Image Slider/script.js
+++ JavaScript Projects CodingNepal/17 Image Slider/script.js
@@ -3,3 +3,5 @@ const wrapper = document.querySelector(".wrapper"),
   carousel = document.querySelector(".carousel"),
   images = document.querySelectorAll("img"),
   buttons = document.querySelectorAll(".button");
+  let imageIndex = 1,
+  intervalId;
