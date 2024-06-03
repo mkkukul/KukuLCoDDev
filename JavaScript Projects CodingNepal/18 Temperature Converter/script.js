e02ca418f7666f064a081918ac5c4d1ca807bc17
mkkukul <mkkukul@gmail.com>
2024-06-03 06:46:49 +0300

    19

--- JavaScript Projects CodingNepal/18 Temperature Converter/script.js
+++ JavaScript Projects CodingNepal/18 Temperature Converter/script.js
@@ -36,3 +36,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
 // Get weather details of passed latitude and longitude
 const getWeatherDetails = (cityName, latitude, longitude) => {
     const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+    fetch(WEATHER_API_URL).then(response => response.json()).then(data => {
+        const forecastArray = data.list;
+        const uniqueForecastDays = new Set();
