ee3bc31ae9506fb61d4fa88e4e62dbda47fbb4ab
mkkukul <mkkukul@gmail.com>
2024-06-03 06:48:42 +0300

    26

--- JavaScript Projects CodingNepal/18 Temperature Converter/script.js
+++ JavaScript Projects CodingNepal/18 Temperature Converter/script.js
@@ -4,34 +4,39 @@ const currentWeatherDiv = document.querySelector(".current-weather");
 const daysForecastDiv = document.querySelector(".days-forecast");
 
 const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API here
+
 // Create weather card HTML based on weather data
 const createWeatherCard = (cityName, weatherItem, index) => {
   if (index === 0) {
     return `<div class="mt-3 d-flex justify-content-between">
-        <div>
-            <h3 class="fw-bold">${cityName} (${
+                    <div>
+                        <h3 class="fw-bold">${cityName} (${
       weatherItem.dt_txt.split(" ")[0]
     })</h3>
-            <h6 class="my-3 mt-3">Temperature: ${(
-              weatherItem.main.temp - 273.15
-            ).toFixed(2)}°C</h6>
-            <h6 class="my-3">Wind: ${weatherItem.wind.speed} M/S</h6>
-            <h6 class="my-3">Humidity: ${weatherItem.main.humidity}%</h6>
-        </div>
-        <div class="text-center me-lg-5">
-        <img src="https://openweathermap.org/img/wn/${
-          weatherItem.weather[0].icon
-        }@4x.png" alt="weather icon">
-        <h6>${weatherItem.weather[0].description}</h6>
-    </div>
-</div>`;
+                        <h6 class="my-3 mt-3">Temperature: ${(
+                          weatherItem.main.temp - 273.15
+                        ).toFixed(2)}°C</h6>
+                        <h6 class="my-3">Wind: ${
+                          weatherItem.wind.speed
+                        } M/S</h6>
+                        <h6 class="my-3">Humidity: ${
+                          weatherItem.main.humidity
+                        }%</h6>
+                    </div>
+                    <div class="text-center me-lg-5">
+                        <img src="https://openweathermap.org/img/wn/${
+                          weatherItem.weather[0].icon
+                        }@4x.png" alt="weather icon">
+                        <h6>${weatherItem.weather[0].description}</h6>
+                    </div>
+                </div>`;
   } else {
     return `<div class="col mb-3">
-    <div class="card border-0 bg-secondary text-white">
+                    <div class="card border-0 bg-secondary text-white">
                         <div class="card-body p-3 text-white">
-                        <h5 class="card-title fw-semibold">(${
-                          weatherItem.dt_txt.split(" ")[0]
-                        })</h5>
+                            <h5 class="card-title fw-semibold">(${
+                              weatherItem.dt_txt.split(" ")[0]
+                            })</h5>
                             <img src="https://openweathermap.org/img/wn/${
                               weatherItem.weather[0].icon
                             }.png" alt="weather icon">
@@ -49,6 +54,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
                 </div>`;
   }
 };
+
 // Get weather details of passed latitude and longitude
 const getWeatherDetails = (cityName, latitude, longitude) => {
   const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
@@ -58,6 +64,7 @@ const getWeatherDetails = (cityName, latitude, longitude) => {
     .then((data) => {
       const forecastArray = data.list;
       const uniqueForecastDays = new Set();
+
       const fiveDaysForecast = forecastArray.filter((forecast) => {
         const forecastDate = new Date(forecast.dt_txt).getDate();
         if (
@@ -69,9 +76,11 @@ const getWeatherDetails = (cityName, latitude, longitude) => {
         }
         return false;
       });
+
       cityInput.value = "";
       currentWeatherDiv.innerHTML = "";
       daysForecastDiv.innerHTML = "";
+
       fiveDaysForecast.forEach((weatherItem, index) => {
         const html = createWeatherCard(cityName, weatherItem, index);
         if (index === 0) {
@@ -85,11 +94,13 @@ const getWeatherDetails = (cityName, latitude, longitude) => {
       alert("An error occurred while fetching the weather forecast!");
     });
 };
+
 // Get coordinates of entered city name
 const getCityCoordinates = () => {
   const cityName = cityInput.value.trim();
   if (cityName === "") return;
   const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
   fetch(API_URL)
     .then((response) => response.json())
     .then((data) => {
@@ -101,3 +112,5 @@ const getCityCoordinates = () => {
       alert("An error occurred while fetching the coordinates!");
     });
 };
+
+searchButton.addEventListener("click", () => getCityCoordinates());
