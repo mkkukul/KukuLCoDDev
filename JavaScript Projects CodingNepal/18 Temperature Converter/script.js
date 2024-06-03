67e8ff26fdd125555baa2dc5b8ca11956ef5917e
mkkukul <mkkukul@gmail.com>
2024-06-03 06:43:39 +0300

    9

--- JavaScript Projects CodingNepal/18 Temperature Converter/script.js
+++ JavaScript Projects CodingNepal/18 Temperature Converter/script.js
@@ -2,3 +2,5 @@ const cityInput = document.querySelector("#city-input");
 const searchButton = document.querySelector("#search-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const daysForecastDiv = document.querySelector(".days-forecast");
+
+const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API here
