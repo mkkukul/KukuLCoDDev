65c26561b2565a12eadbdc45df5ad1ef7756fbe3
mkkukul <mkkukul@gmail.com>
2024-06-03 06:48:16 +0300

    25

--- JavaScript Projects CodingNepal/18 Temperature Converter/script.js
+++ JavaScript Projects CodingNepal/18 Temperature Converter/script.js
@@ -87,6 +87,17 @@ const getWeatherDetails = (cityName, latitude, longitude) => {
 };
 // Get coordinates of entered city name
 const getCityCoordinates = () => {
-    const cityName = cityInput.value.trim();
-    if (cityName === "") return;
-    const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+  const cityName = cityInput.value.trim();
+  if (cityName === "") return;
+  const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data.length) return alert(`No coordinates found for ${cityName}`);
+      const { lat, lon, name } = data[0];
+      getWeatherDetails(name, lat, lon);
+    })
+    .catch(() => {
+      alert("An error occurred while fetching the coordinates!");
+    });
+};
