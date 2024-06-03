d931565e5166753efd6484ca1c9e39e161fec5be
mkkukul <mkkukul@gmail.com>
2024-06-03 06:44:11 +0300

    11

--- JavaScript Projects CodingNepal/18 Temperature Converter/script.js
+++ JavaScript Projects CodingNepal/18 Temperature Converter/script.js
@@ -8,3 +8,9 @@ const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API here
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) {
         return `<div class="mt-3 d-flex justify-content-between">
+        <div>
+                        <h3 class="fw-bold">${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h3>
+                        <h6 class="my-3 mt-3">Temperature: ${((weatherItem.main.temp - 273.15).toFixed(2))}°C</h6>
+                        <h6 class="my-3">Wind: ${weatherItem.wind.speed} M/S</h6>
+                        <h6 class="my-3">Humidity: ${weatherItem.main.humidity}%</h6>
+                    </div>
