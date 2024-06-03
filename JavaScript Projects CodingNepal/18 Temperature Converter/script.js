0363c332f29cfe74e53cd5b9df2d2b1489a9324d
mkkukul <mkkukul@gmail.com>
2024-06-03 06:43:49 +0300

    10

--- JavaScript Projects CodingNepal/18 Temperature Converter/script.js
+++ JavaScript Projects CodingNepal/18 Temperature Converter/script.js
@@ -4,3 +4,7 @@ const currentWeatherDiv = document.querySelector(".current-weather");
 const daysForecastDiv = document.querySelector(".days-forecast");
 
 const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API here
+// Create weather card HTML based on weather data
+const createWeatherCard = (cityName, weatherItem, index) => {
+    if(index === 0) {
+        return `<div class="mt-3 d-flex justify-content-between">
