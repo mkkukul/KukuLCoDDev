ab5b81de3303155e61edcfbb2c16b865ad1b7b08
mkkukul <mkkukul@gmail.com>
2024-06-04 09:53:14 +0300

    25

--- JavaScript Projects CodingNepal/19 Double Click Heart Animation/script.js
+++ JavaScript Projects CodingNepal/19 Double Click Heart Animation/script.js
@@ -7,11 +7,14 @@ container.addEventListener("dblclick", (e) => {
   // Calculate the x and y position of the double-click event
   let xValue = e.clientX - e.target.offsetLeft,
     yValue = e.clientY - e.target.offsetTop;
+
   // Set the position of the heart element using the x and y values
   heart.style.left = `${xValue}px`;
   heart.style.top = `${yValue}px`;
+
   // Add the active class to the heart element to animate it
   heart.classList.add("active");
+
   // Remove the active class after 1 second
   setTimeout(() => {
     heart.classList.remove("active");
