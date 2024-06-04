920f56a8f263c953a6a0f3302f07e6ae21a42f66
mkkukul <mkkukul@gmail.com>
2024-06-04 09:50:50 +0300

    15

--- JavaScript Projects CodingNepal/19 Double Click Heart Animation/script.js
+++ JavaScript Projects CodingNepal/19 Double Click Heart Animation/script.js
@@ -3,3 +3,5 @@ const container = document.querySelector(".container"),
   heart = document.querySelector(".heart");
 // Add a double-click event listener to the container
 container.addEventListener("dblclick", (e) => {
+    // Calculate the x and y position of the double-click event
+  let xValue = e.clientX - e.target.offsetLeft,
