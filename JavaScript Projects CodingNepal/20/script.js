dbf52964fd228cf9df18086cca0849eaab021a7a
mkkukul <mkkukul@gmail.com>
2024-06-05 05:44:56 +0300

    26

--- JavaScript Projects CodingNepal/20/script.js
+++ JavaScript Projects CodingNepal/20/script.js
@@ -50,28 +50,42 @@ controls.forEach((button) =>
   )
 );
 const initGame = () => {
-    if(gameOver) return handleGameOver();
-    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
-    // Checking if the snake hit the food
-    if(snakeX === foodX && snakeY === foodY) {
-        updateFoodPosition();
-        snakeBody.push([foodY, foodX]); // Pushing food position to snake body array
-        score++; // increment score by 1
-        highScore = score >= highScore ? score : highScore;
-        localStorage.setItem("high-score", highScore);
-        scoreElement.innerText = `Score: ${score}`;
-        highScoreElement.innerText = `High Score: ${highScore}`;
-    }
-    // Updating the snake's head position based on the current velocity
-    snakeX += velocityX;
-    snakeY += velocityY;
+  if (gameOver) return handleGameOver();
+  let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+  // Checking if the snake hit the food
+  if (snakeX === foodX && snakeY === foodY) {
+    updateFoodPosition();
+    snakeBody.push([foodY, foodX]); // Pushing food position to snake body array
+    score++; // increment score by 1
+    highScore = score >= highScore ? score : highScore;
+    localStorage.setItem("high-score", highScore);
+    scoreElement.innerText = `Score: ${score}`;
+    highScoreElement.innerText = `High Score: ${highScore}`;
+  }
+  // Updating the snake's head position based on the current velocity
+  snakeX += velocityX;
+  snakeY += velocityY;
 
-     // Shifting forward the values of the elements in the snake body by one
-     for (let i = snakeBody.length - 1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i - 1];
+  // Shifting forward the values of the elements in the snake body by one
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+  snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
+  // Checking if the snake's head is out of wall, if so setting gameOver to true
+  if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+    return (gameOver = true);
+  }
+  for (let i = 0; i < snakeBody.length; i++) {
+    // Adding a div for each part of the snake's body
+    html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+    // Checking if the snake head hit the body, if so set gameOver to true
+    if (
+      i !== 0 &&
+      snakeBody[0][1] === snakeBody[i][1] &&
+      snakeBody[0][0] === snakeBody[i][0]
+    ) {
+      gameOver = true;
     }
-    snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
-    // Checking if the snake's head is out of wall, if so setting gameOver to true
-    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
-        return gameOver = true;
-    }
+  }
+  playBoard.innerHTML = html;
+};
