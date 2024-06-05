b8cce28933523e413bf49e0a795b60ffdc00b846
mkkukul <mkkukul@gmail.com>
2024-06-05 05:38:21 +0300

    12

--- JavaScript Projects CodingNepal/20/script.js
+++ JavaScript Projects CodingNepal/20/script.js
@@ -1,2 +1,4 @@
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
