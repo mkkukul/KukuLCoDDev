7a4e418cb2942b9bc0466f6dac0f81670b6598ab
mkkukul <mkkukul@gmail.com>
2024-06-05 05:38:45 +0300

    14

--- JavaScript Projects CodingNepal/20/script.js
+++ JavaScript Projects CodingNepal/20/script.js
@@ -5,3 +5,10 @@ const controls = document.querySelectorAll(".controls i");
 
 let gameOver = false;
 let foodX, foodY;
+let snakeX = 5,
+  snakeY = 5;
+let velocityX = 0,
+  velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
