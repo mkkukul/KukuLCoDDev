e41318c27ac6c67031e39ea74f887449dba6add2
mkkukul <mkkukul@gmail.com>
2024-06-06 21:51:37 +0300

    14

--- JavaScript Projects CodingNepal/21/script.js
+++ JavaScript Projects CodingNepal/21/script.js
@@ -1,3 +1,7 @@
 //DOM Elements
 const boxes = document.querySelectorAll(".box"),
   image = document.querySelector(".image");
+//Loop through each boxes element
+boxes.forEach((box) => {
+    //When a draggable element dragged over a box element
+    box.addEventListener("dragover", (e) => 
