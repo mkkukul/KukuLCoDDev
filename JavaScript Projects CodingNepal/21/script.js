2305e9dead3c55271892bedc6a26eefd51adf292
mkkukul <mkkukul@gmail.com>
2024-06-06 21:51:48 +0300

    15

--- JavaScript Projects CodingNepal/21/script.js
+++ JavaScript Projects CodingNepal/21/script.js
@@ -4,4 +4,7 @@ const boxes = document.querySelectorAll(".box"),
 //Loop through each boxes element
 boxes.forEach((box) => {
     //When a draggable element dragged over a box element
-    box.addEventListener("dragover", (e) => 
+    box.addEventListener("dragover", (e) =>
+        e.preventDefault(); //Prevent default behaviour
+    box.classList.add("hovered");
+  }); 
