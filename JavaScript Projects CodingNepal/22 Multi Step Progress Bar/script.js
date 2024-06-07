1bc91ef5e52b11811e31d953a50c5a44dea857b1
mkkukul <mkkukul@gmail.com>
2024-06-07 11:28:31 +0300

    17

--- JavaScript Projects CodingNepal/22 Multi Step Progress Bar/script.js
+++ JavaScript Projects CodingNepal/22 Multi Step Progress Bar/script.js
@@ -2,3 +2,7 @@
 const circles = document.querySelectorAll(".circle"),
   progressBar = document.querySelector(".indicator"),
   buttons = document.querySelectorAll("button");
+
+  let currentStep = 1;
+// function that updates the current step and updates the DOM
+const updateSteps = (e) => {
