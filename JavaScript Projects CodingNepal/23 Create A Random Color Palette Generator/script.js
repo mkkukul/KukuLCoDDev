f91ca91322f240f48c4fd38cd83095bb3af9e641
mkkukul <mkkukul@gmail.com>
2024-06-08 15:34:56 +0300

    26

--- JavaScript Projects CodingNepal/23 Create A Random Color Palette Generator/script.js
+++ JavaScript Projects CodingNepal/23 Create A Random Color Palette Generator/script.js
@@ -2,12 +2,14 @@ const container = document.querySelector(".container");
 const refreshBtn = document.querySelector(".refresh-btn");
 
 const maxPaletteBoxes = 32;
+
 const generatePalette = () => {
   container.innerHTML = ""; // clearing the container
   for (let i = 0; i < maxPaletteBoxes; i++) {
     // generating a random hex color code
     let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
     randomHex = `#${randomHex.padStart(6, "0")}`;
+
     // creating a new 'li' element and inserting it to the container
     const color = document.createElement("li");
     color.classList.add("color");
@@ -19,12 +21,18 @@ const generatePalette = () => {
   }
 };
 generatePalette();
+
 const copyColor = (elem, hexVal) => {
-    const colorElement = elem.querySelector(".hex-value");
-    // Copying the hex value, updating the text to copied, 
-    // and changing text back to original hex value after 1 second
-    navigator.clipboard.writeText(hexVal).then(() => {
-        colorElement.innerText = "Copied";
-        setTimeout(() => colorElement.innerText = hexVal, 1000);
-    }).catch(() => alert("Failed to copy the color code!")); // showing alert if color can't be copied
-    refreshBtn.addEventListener("click", generatePalette);
+  const colorElement = elem.querySelector(".hex-value");
+  // Copying the hex value, updating the text to copied,
+  // and changing text back to original hex value after 1 second
+  navigator.clipboard
+    .writeText(hexVal)
+    .then(() => {
+      colorElement.innerText = "Copied";
+      setTimeout(() => (colorElement.innerText = hexVal), 1000);
+    })
+    .catch(() => alert("Failed to copy the color code!")); // showing alert if color can't be copied
+};
+
+refreshBtn.addEventListener("click", generatePalette);
