9fcf397f0b1f6af94b782904b35891ecd56339a6
mkkukul <mkkukul@gmail.com>
2024-06-08 15:33:05 +0300

    19

--- JavaScript Projects CodingNepal/23 Create A Random Color Palette Generator/script.js
+++ JavaScript Projects CodingNepal/23 Create A Random Color Palette Generator/script.js
@@ -4,4 +4,7 @@ const refreshBtn = document.querySelector(".refresh-btn");
 const maxPaletteBoxes = 32;
 const generatePalette = () => {
     container.innerHTML = ""; // clearing the container
-    
+    for (let i = 0; i < maxPaletteBoxes; i++) {
+        // generating a random hex color code
+        let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+        randomHex = `#${randomHex.padStart(6, "0")}`;
