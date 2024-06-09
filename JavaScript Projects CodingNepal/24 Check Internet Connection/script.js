d49f42a05f66f85721fdfedd57d99288414b8b27
mkkukul <mkkukul@gmail.com>
2024-06-09 07:36:51 +0300

    16

--- JavaScript Projects CodingNepal/24 Check Internet Connection/script.js
+++ JavaScript Projects CodingNepal/24 Check Internet Connection/script.js
@@ -1,2 +1,5 @@
 const popup = document.querySelector(".popup"),
-wifiIcon = document.querySelector(".icon i"),
+  wifiIcon = document.querySelector(".icon i"),
+  popupTitle = document.querySelector(".popup .title"),
+  popupDesc = document.querySelector(".desc"),
+  reconnectBtn = document.querySelector(".reconnect");
