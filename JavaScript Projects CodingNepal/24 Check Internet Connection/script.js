c0903db104251e4e8180cdd78fd8c6baeed0e608
mkkukul <mkkukul@gmail.com>
2024-06-09 07:39:58 +0300

    25

--- JavaScript Projects CodingNepal/24 Check Internet Connection/script.js
+++ JavaScript Projects CodingNepal/24 Check Internet Connection/script.js
@@ -21,15 +21,25 @@ const checkConnection = async () => {
   handlePopup(isOnline);
 };
 const handlePopup = (status) => {
-    if(status) { // If the status is true (online), update icon, title, and description accordingly
-        wifiIcon.className = "uil uil-wifi";
-        popupTitle.innerText = "Restored Connection";
-        popupDesc.innerHTML = "Your device is now successfully connected to the internet.";
-        popup.classList.add("online");
-        return setTimeout(() => popup.classList.remove("show"), 2000);
-    }
-    // If the status is false (offline), update the icon, title, and description accordingly
-    wifiIcon.className = "uil uil-wifi-slash";
-    popupTitle.innerText = "Lost Connection";
-    popupDesc.innerHTML = "Your network is unavailable. We will attempt to reconnect you in <b>10</b> seconds.";
-    popup.className = "popup show";
+  if (status) {
+    // If the status is true (online), update icon, title, and description accordingly
+    wifiIcon.className = "uil uil-wifi";
+    popupTitle.innerText = "Restored Connection";
+    popupDesc.innerHTML =
+      "Your device is now successfully connected to the internet.";
+    popup.classList.add("online");
+    return setTimeout(() => popup.classList.remove("show"), 2000);
+  }
+  // If the status is false (offline), update the icon, title, and description accordingly
+  wifiIcon.className = "uil uil-wifi-slash";
+  popupTitle.innerText = "Lost Connection";
+  popupDesc.innerHTML =
+    "Your network is unavailable. We will attempt to reconnect you in <b>10</b> seconds.";
+  popup.className = "popup show";
+  intervalId = setInterval(() => {
+    // Set an interval to decrease the timer by 1 every second
+    timer--;
+    if (timer === 0) checkConnection(); // If the timer reaches 0, check the connection again
+    popup.querySelector(".desc b").innerText = timer;
+  }, 1000);
+};
