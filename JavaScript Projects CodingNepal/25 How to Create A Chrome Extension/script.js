e195336835f12971b8cd0932add69a88b657030d
mkkukul <mkkukul@gmail.com>
2024-06-10 14:30:35 +0300

    24

--- JavaScript Projects CodingNepal/25 How to Create A Chrome Extension/script.js
+++ JavaScript Projects CodingNepal/25 How to Create A Chrome Extension/script.js
@@ -33,6 +33,22 @@ const showColor = () => {
 };
 showColor();
 const activateEyeDropper = () => {
-    document.body.style.display = "none";
-    setTimeout(async () => {
-        try {
+  document.body.style.display = "none";
+  setTimeout(async () => {
+    try {
+      // Opening the eye dropper and getting the selected color
+      const eyeDropper = new EyeDropper();
+      const { sRGBHex } = await eyeDropper.open();
+      navigator.clipboard.writeText(sRGBHex);
+      // Adding the color to the list if it doesn't already exist
+      if (!pickedColors.includes(sRGBHex)) {
+        pickedColors.push(sRGBHex);
+        localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
+        showColor();
+      }
+    } catch (error) {
+      alert("Failed to copy the color code!");
+    }
+    document.body.style.display = "block";
+  }, 10);
+};
