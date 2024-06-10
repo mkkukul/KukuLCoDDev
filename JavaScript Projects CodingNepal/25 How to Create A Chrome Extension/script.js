739669a89e0874d35ae6fc745a931bc323a13fab
mkkukul <mkkukul@gmail.com>
2024-06-10 14:28:48 +0300

    19

--- JavaScript Projects CodingNepal/25 How to Create A Chrome Extension/script.js
+++ JavaScript Projects CodingNepal/25 How to Create A Chrome Extension/script.js
@@ -2,3 +2,10 @@ const colorPickerBtn = document.querySelector("#color-picker");
 const clearAll = document.querySelector(".clear-all");
 const colorList = document.querySelector(".all-colors");
 const pickedColors = JSON.parse(localStorage.getItem("picked-colors") || "[]");
+
+// Copying the color code to the clipboard and updating the element text
+const copyColor = (elem) => {
+  elem.innerText = "Copied";
+  navigator.clipboard.writeText(elem.dataset.color);
+  setTimeout(() => (elem.innerText = elem.dataset.color), 1000);
+};
