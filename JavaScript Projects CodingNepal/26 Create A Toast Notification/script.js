9ad959ad2e6dfebc00e9eee8d0f9a5a23698cd17
mkkukul <mkkukul@gmail.com>
2024-06-11 13:00:14 +0300

    17

--- JavaScript Projects CodingNepal/26 Create A Toast Notification/script.js
+++ JavaScript Projects CodingNepal/26 Create A Toast Notification/script.js
@@ -1,2 +1,10 @@
 const notifications = document.querySelector(".notifications"),
   buttons = document.querySelectorAll(".buttons .btn");
+
+  // Object containing details for different types of toasts
+const toastDetails = {
+    timer: 5000,
+    success: {
+        icon: 'fa-circle-check',
+        text: 'Success: This is a success toast.',
+    },
