ed6bfa76c499f954ea716edd468280e31c787ae6
mkkukul <mkkukul@gmail.com>
2024-06-11 13:01:12 +0300

    22

--- JavaScript Projects CodingNepal/26 Create A Toast Notification/script.js
+++ JavaScript Projects CodingNepal/26 Create A Toast Notification/script.js
@@ -26,3 +26,9 @@ const removeToast = (toast) => {
   if (toast.timeoutId) clearTimeout(toast.timeoutId); // Clearing the timeout for the toast
   setTimeout(() => toast.remove(), 500); // Removing the toast after 500ms
 };
+const createToast = (id) => {
+    // Getting the icon and text for the toast based on the id passed
+    const { icon, text } = toastDetails[id];
+    const toast = document.createElement("li"); // Creating a new 'li' element for the toast
+    toast.className = `toast ${id}`; // Setting the classes for the toast
+    // Setting the inner HTML for the toast
