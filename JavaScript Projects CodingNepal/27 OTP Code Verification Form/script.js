a230dce2b4276a0f7f1a2ecc70c17677c8d8418f
mkkukul <mkkukul@gmail.com>
2024-06-12 08:24:40 +0300

    17

--- JavaScript Projects CodingNepal/27 OTP Code Verification Form/script.js
+++ JavaScript Projects CodingNepal/27 OTP Code Verification Form/script.js
@@ -1,2 +1,8 @@
 const inputs = document.querySelectorAll("input"),
   button = document.querySelector("button");
+// iterate over all inputs
+inputs.forEach((input, index1) => {
+    input.addEventListener("keyup", (e) => {
+      // This code gets the current input element and stores it in the currentInput variable
+      // This code gets the next sibling element of the current input element and stores it in the nextInput variable
+      // This code gets the previous sibling element of the current input element and stores it in the prevInput variable
