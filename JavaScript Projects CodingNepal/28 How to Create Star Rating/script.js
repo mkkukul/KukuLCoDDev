680648738e5e3781ffdff791ff29dea3ecac22e7
mkkukul <mkkukul@gmail.com>
2024-06-13 06:14:52 +0300

    16

---
+++ JavaScript Projects CodingNepal/28 How to Create Star Rating/script.js
@@ -0,0 +1,4 @@
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars = document.querySelectorAll(".stars i");
+// Loop through the "stars" NodeList
+stars.forEach((star, index1) => {
