62ea06c07cf75c691f4dbacceb81e52d05f95c03
mkkukul <mkkukul@gmail.com>
2024-06-13 06:15:03 +0300

    17

--- JavaScript Projects CodingNepal/28 How to Create Star Rating/script.js
+++ JavaScript Projects CodingNepal/28 How to Create Star Rating/script.js
@@ -2,3 +2,5 @@
 const stars = document.querySelectorAll(".stars i");
 // Loop through the "stars" NodeList
 stars.forEach((star, index1) => {
+    / Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
