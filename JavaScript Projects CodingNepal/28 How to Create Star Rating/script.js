1271cf0279c79545122706b509787212fd658f88
mkkukul <mkkukul@gmail.com>
2024-06-13 06:18:01 +0300

    24

--- JavaScript Projects CodingNepal/28 How to Create Star Rating/script.js
+++ JavaScript Projects CodingNepal/28 How to Create Star Rating/script.js
@@ -9,7 +9,9 @@ stars.forEach((star, index1) => {
     stars.forEach((star, index2) => {
       // Add the "active" class to the clicked star and any stars with a lower index
       // and remove the "active" class from any stars with a higher index
-      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+      index1 >= index2
+        ? star.classList.add("active")
+        : star.classList.remove("active");
     });
   });
 });
