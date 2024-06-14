5da67fb0b6977cf5a91f83100762ed4a2b10114f
mkkukul <mkkukul@gmail.com>
2024-06-14 22:18:13 +0300

    23

--- JavaScript Projects CodingNepal/29 Build A Playable PIANO/script.js
+++ JavaScript Projects CodingNepal/29 Build A Playable PIANO/script.js
@@ -5,8 +5,13 @@ const pianoKeys = document.querySelectorAll(".piano-keys .key"),
 let allKeys = [],
   audio = new Audio(`tunes/a.wav`); // by default, audio src is "a" tune
 
-  const playTune = (key) => {
-    audio.src = `tunes/${key}.wav`; // passing audio src based on key pressed 
-    audio.play(); // playing audio
-    const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
-    clickedKey.classList.add("active"); // adding active class to the clicked key element
+const playTune = (key) => {
+  audio.src = `tunes/${key}.wav`; // passing audio src based on key pressed
+  audio.play(); // playing audio
+  const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
+  clickedKey.classList.add("active"); // adding active class to the clicked key element
+  setTimeout(() => {
+    // removing active class after 150 ms from the clicked key element
+    clickedKey.classList.remove("active");
+  }, 150);
+};
