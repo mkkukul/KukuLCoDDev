e490d1360b2bde6448a39f3162062d354d0277d1
mkkukul <mkkukul@gmail.com>
2024-06-14 22:17:34 +0300

    19

--- JavaScript Projects CodingNepal/29 Build A Playable PIANO/script.js
+++ JavaScript Projects CodingNepal/29 Build A Playable PIANO/script.js
@@ -1,3 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key"),
   volumeSlider = document.querySelector(".volume-slider input"),
   keysCheckbox = document.querySelector(".keys-checkbox input");
+
+let allKeys = [],
+  audio = new Audio(`tunes/a.wav`); // by default, audio src is "a" tune
