146ba52403fb68f73c424144822f7966acdf8d01
mkkukul <mkkukul@gmail.com>
2024-06-14 22:19:00 +0300

    27

--- JavaScript Projects CodingNepal/29 Build A Playable PIANO/script.js
+++ JavaScript Projects CodingNepal/29 Build A Playable PIANO/script.js
@@ -27,3 +27,10 @@ const showHideKeys = () => {
   // toggling hide class from each key on the checkbox click
   pianoKeys.forEach((key) => key.classList.toggle("hide"));
 };
+const pressedKey = (e) => {
+  // if the pressed key is in the allKeys array, only call the playTune function
+  if (allKeys.includes(e.key)) playTune(e.key);
+};
+keysCheckbox.addEventListener("click", showHideKeys);
+volumeSlider.addEventListener("input", handleVolume);
+document.addEventListener("keydown", pressedKey);
