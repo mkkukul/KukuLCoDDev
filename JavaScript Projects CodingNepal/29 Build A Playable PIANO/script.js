cda48f636715a98a44aa5f8845d33ab782c7d370
mkkukul <mkkukul@gmail.com>
2024-06-14 22:19:10 +0300

    28

--- JavaScript Projects CodingNepal/29 Build A Playable PIANO/script.js
+++ JavaScript Projects CodingNepal/29 Build A Playable PIANO/script.js
@@ -8,6 +8,7 @@ let allKeys = [],
 const playTune = (key) => {
   audio.src = `tunes/${key}.wav`; // passing audio src based on key pressed
   audio.play(); // playing audio
+
   const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
   clickedKey.classList.add("active"); // adding active class to the clicked key element
   setTimeout(() => {
@@ -15,22 +16,27 @@ const playTune = (key) => {
     clickedKey.classList.remove("active");
   }, 150);
 };
+
 pianoKeys.forEach((key) => {
   allKeys.push(key.dataset.key); // adding data-key value to the allKeys array
   // calling playTune function with passing data-key value as an argument
   key.addEventListener("click", () => playTune(key.dataset.key));
 });
+
 const handleVolume = (e) => {
   audio.volume = e.target.value; // passing the range slider value as an audio volume
 };
+
 const showHideKeys = () => {
   // toggling hide class from each key on the checkbox click
   pianoKeys.forEach((key) => key.classList.toggle("hide"));
 };
+
 const pressedKey = (e) => {
   // if the pressed key is in the allKeys array, only call the playTune function
   if (allKeys.includes(e.key)) playTune(e.key);
 };
+
 keysCheckbox.addEventListener("click", showHideKeys);
 volumeSlider.addEventListener("input", handleVolume);
 document.addEventListener("keydown", pressedKey);
