54150bcd6e32bbb4eb29201937b90c25dea2886c
mkkukul <mkkukul@gmail.com>
2024-06-15 07:03:12 +0300

    22

--- JavaScript Projects CodingNepal/script.js
+++ JavaScript Projects CodingNepal/script.js
@@ -9,3 +9,6 @@ const screenshotBtn = document.querySelector("#src-btn"),
         video.addEventListener("loadedmetadata", () => {
             const canvas = document.createElement("canvas");
             const ctx = canvas.getContext("2d");
+            // passing video width & height as canvas width & height
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
