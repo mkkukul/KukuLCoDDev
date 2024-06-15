ce16c5bca74990bb8cb3028eeb6ae746e974e851
mkkukul <mkkukul@gmail.com>
2024-06-15 07:04:46 +0300

    28

--- JavaScript Projects CodingNepal/script.js
+++ JavaScript Projects CodingNepal/script.js
@@ -1,6 +1,7 @@
 const screenshotBtn = document.querySelector("#src-btn"),
   screenshotPreview = document.querySelector(".src-preview"),
   closeBtn = screenshotPreview.querySelector("#close-btn");
+
 const captureScreen = async () => {
   try {
     // asking permission to use a media input to record current tab
@@ -8,16 +9,20 @@ const captureScreen = async () => {
       preferCurrentTab: true,
     });
     const video = document.createElement("video");
+
     video.addEventListener("loadedmetadata", () => {
       const canvas = document.createElement("canvas");
       const ctx = canvas.getContext("2d");
+
       // passing video width & height as canvas width & height
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
+
       video.play(); // playing the video so the drawn image won't be black or blank
       // drawing an image of the captured video stream
       ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
       stream.getVideoTracks()[0].stop(); // terminating first video track of the stream
+
       // passing canvas data url as screenshot preview src
       screenshotPreview.querySelector("img").src = canvas.toDataURL();
       screenshotPreview.classList.add("show");
@@ -28,6 +33,7 @@ const captureScreen = async () => {
     alert("Failed to capture screenshot!");
   }
 };
+
 closeBtn.addEventListener("click", () =>
   screenshotPreview.classList.toggle("show")
 );
