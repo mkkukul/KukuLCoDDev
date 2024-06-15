35b3e80c832f98d1478ab280244768b42c041668
mkkukul <mkkukul@gmail.com>
2024-06-15 07:02:58 +0300

    21

--- JavaScript Projects CodingNepal/script.js
+++ JavaScript Projects CodingNepal/script.js
@@ -5,3 +5,7 @@ const screenshotBtn = document.querySelector("#src-btn"),
     try {
         // asking permission to use a media input to record current tab
         const stream = await navigator.mediaDevices.getDisplayMedia({ preferCurrentTab: true });
+        const video = document.createElement("video");
+        video.addEventListener("loadedmetadata", () => {
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
