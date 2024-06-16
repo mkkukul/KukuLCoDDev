5fa889de1c2af20a0b9edc2eac478b50c8618de7
mkkukul <mkkukul@gmail.com>
2024-06-16 07:21:16 +0300

    20

--- JavaScript Projects CodingNepal/31 Create a Responsive Navigation Bar with Search Box/script.js
+++ JavaScript Projects CodingNepal/31 Create a Responsive Navigation Bar with Search Box/script.js
@@ -15,3 +15,6 @@ navOpenBtn.addEventListener("click", () => {
   nav.classList.remove("openSearch");
   searchIcon.classList.replace("uil-times", "uil-search");
 });
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
