7da4afd6cbd594428089aa4c6c77d52e1b05123a
mkkukul <mkkukul@gmail.com>
2024-06-18 08:43:53 +0300

    15

--- JavaScript Projects CodingNepal/32 Create A Download Button with Timer/script.js
+++ JavaScript Projects CodingNepal/32 Create A Download Button with Timer/script.js
@@ -3,3 +3,5 @@ const fileLink = "https://drive.google.com/uc?
 export=download&id=1aYiaLn3YOjL-_o5QBCy7tU1epqA6gZoi";
 const initTimer = () => {
     if(downloadBtn.classList.contains("disable-timer")) {
+        return location.href = fileLink;
+    }
