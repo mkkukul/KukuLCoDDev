e7b32c1d4d356d18fda65b20e0701c2ff6933923
mkkukul <mkkukul@gmail.com>
2024-06-20 22:27:10 +0300

    21

--- JavaScript Projects CodingNepal/33 Draggable Slider Tabs Like YouTube/script.js
+++ JavaScript Projects CodingNepal/33 Draggable Slider Tabs Like YouTube/script.js
@@ -1,7 +1,10 @@
 const tabsBox = document.querySelector(".tabs-box"),
   allTabs = tabsBox.querySelectorAll(".tab"),
   arrowIcons = document.querySelectorAll(".icon i");
-  let isDragging = false;
-  const handleIcons = (scrollVal) => {
-      let maxScrollableWidth = tabsBox.scrollWidth - tabsBox.clientWidth;
-      arrowIcons[0].parentElement.style.display = scrollVal <= 0 ? "none" : "flex";
+let isDragging = false;
+const handleIcons = (scrollVal) => {
+  let maxScrollableWidth = tabsBox.scrollWidth - tabsBox.clientWidth;
+  arrowIcons[0].parentElement.style.display = scrollVal <= 0 ? "none" : "flex";
+  arrowIcons[1].parentElement.style.display =
+    maxScrollableWidth - scrollVal <= 1 ? "none" : "flex";
+};
