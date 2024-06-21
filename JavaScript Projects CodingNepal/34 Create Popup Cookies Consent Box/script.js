ea5258cea8349eac3e0c22fb6ecd9dd5c376e255
mkkukul <mkkukul@gmail.com>
2024-06-21 10:41:22 +0300

    17

--- JavaScript Projects CodingNepal/34 Create Popup Cookies Consent Box/script.js
+++ JavaScript Projects CodingNepal/34 Create Popup Cookies Consent Box/script.js
@@ -2,3 +2,5 @@ const cookieBox = document.querySelector(".wrapper"),
   buttons = document.querySelectorAll(".button");
   const executeCodes = () => {
     //if cookie contains codinglab it will be returned and below of this code will not run
+    if (document.cookie.includes("codinglab")) return;
+  cookieBox.classList.add("show");
