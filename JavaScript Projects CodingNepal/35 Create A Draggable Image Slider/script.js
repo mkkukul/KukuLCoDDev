3657969949b92bcdf3f02cec511e2aa2150d4bb3
mkkukul <mkkukul@gmail.com>
2024-06-22 07:49:51 +0300

    18

--- JavaScript Projects CodingNepal/35 Create A Draggable Image Slider/script.js
+++ JavaScript Projects CodingNepal/35 Create A Draggable Image Slider/script.js
@@ -1,3 +1,8 @@
 const carousel = document.querySelector(".carousel"),
   firstImg = carousel.querySelectorAll("img")[0],
   arrowIcons = document.querySelectorAll(".wrapper i");
+let isDragStart = false,
+  isDragging = false,
+  prevPageX,
+  prevScrollLeft,
+  positionDiff;
