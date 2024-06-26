fa502f039d64fe25d9d21634a4b7fbb3183b9cf4
mkkukul <mkkukul@gmail.com>
2024-06-26 22:13:20 +0300

    20

--- JavaScript Projects CodingNepal/37 Create Random Password Generator/script.js
+++ JavaScript Projects CodingNepal/37 Create Random Password Generator/script.js
@@ -1,4 +1,5 @@
 const passwordInput = document.querySelector(".password-box input"),
-copyIcon = document.querySelector(".password-box .copy-icon"),
-rangeInput = document.querySelector(".range-box input"),
+  copyIcon = document.querySelector(".password-box .copy-icon"),
+  rangeInput = document.querySelector(".range-box input"),
   sliderNumber = document.querySelector(".range-box .slider-number"),
+  generateButton = document.querySelector(".generate-button");
