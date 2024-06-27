3527fac790daee5a438603731bc713e62a868e95
mkkukul <mkkukul@gmail.com>
2024-06-27 22:56:56 +0300

    25

--- JavaScript Projects CodingNepal/38 Resize and Compress Images/script.js
+++ JavaScript Projects CodingNepal/38 Resize and Compress Images/script.js
@@ -6,8 +6,16 @@ const uploadBox = document.querySelector(".upload-box"),
   ratioInput = document.querySelector(".ratio input"),
   qualityInput = document.querySelector(".quality input"),
   downloadBtn = document.querySelector(".download-btn");
-  let ogImageRatio;
-  const loadFile = (e) => {
-      const file = e.target.files[0]; // getting first user selected file
-      if(!file) return; // return if user hasn't selected any file
-    previewImg.src = URL.createObjectURL(file); // passing selected file url to preview img src
+let ogImageRatio;
+const loadFile = (e) => {
+  const file = e.target.files[0]; // getting first user selected file
+  if (!file) return; // return if user hasn't selected any file
+  previewImg.src = URL.createObjectURL(file); // passing selected file url to preview img src
+  previewImg.addEventListener("load", () => {
+    // once img loaded
+    widthInput.value = previewImg.naturalWidth;
+    heightInput.value = previewImg.naturalHeight;
+    ogImageRatio = previewImg.naturalWidth / previewImg.naturalHeight;
+    document.querySelector(".wrapper").classList.add("active");
+  });
+};
