f2c559e08aa77edd0dadea66e90e7f315b965efc
mkkukul <mkkukul@gmail.com>
2024-06-27 22:56:04 +0300

    20

--- JavaScript Projects CodingNepal/38 Resize and Compress Images/script.js
+++ JavaScript Projects CodingNepal/38 Resize and Compress Images/script.js
@@ -1,2 +1,4 @@
 const uploadBox = document.querySelector(".upload-box"),
 previewImg = uploadBox.querySelector("img"),
+fileInput = uploadBox.querySelector("input"),
+widthInput = document.querySelector(".width input"),
