2dd7e78abc3519936700a5658e8b4a51d4964169
mkkukul <mkkukul@gmail.com>
2024-06-27 22:57:58 +0300

    30

--- JavaScript Projects CodingNepal/38 Resize and Compress Images/script.js
+++ JavaScript Projects CodingNepal/38 Resize and Compress Images/script.js
@@ -34,14 +34,20 @@ heightInput.addEventListener("keyup", () => {
   widthInput.value = Math.floor(width);
 });
 const resizeAndDownload = () => {
-    const canvas = document.createElement("canvas");
-    const a = document.createElement("a");
-    const ctx = canvas.getContext("2d");
-    // if quality checkbox is checked, pass 0.5 to imgQuality else pass 1.0
-    // 1.0 is 100% quality where 0.5 is 50% of total. you can pass from 0.1 - 1.0
-    const imgQuality = qualityInput.checked ? 0.5 : 1.0;
-    // setting canvas height & width according to the input values
-    canvas.width = widthInput.value;
-    canvas.height = heightInput.value;
-    // drawing user selected image onto the canvas
-    ctx.drawImage(previewImg, 0, 0, canvas.width, canvas.height);
+  const canvas = document.createElement("canvas");
+  const a = document.createElement("a");
+  const ctx = canvas.getContext("2d");
+  // if quality checkbox is checked, pass 0.5 to imgQuality else pass 1.0
+  // 1.0 is 100% quality where 0.5 is 50% of total. you can pass from 0.1 - 1.0
+  const imgQuality = qualityInput.checked ? 0.5 : 1.0;
+  // setting canvas height & width according to the input values
+  canvas.width = widthInput.value;
+  canvas.height = heightInput.value;
+  // drawing user selected image onto the canvas
+  ctx.drawImage(previewImg, 0, 0, canvas.width, canvas.height);
+
+  // passing canvas data url as href value of <a> element
+  a.href = canvas.toDataURL("image/jpeg", imgQuality);
+  a.download = new Date().getTime(); // passing current time as download value
+  a.click(); // clicking <a> element so the file download
+};
