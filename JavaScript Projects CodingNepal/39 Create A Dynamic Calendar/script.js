8c4f0bc2a17acb973f0f262e11af5f833b6e4672
mkkukul <mkkukul@gmail.com>
2024-07-01 21:24:53 +0300

    16

--- JavaScript Projects CodingNepal/39 Create A Dynamic Calendar/script.js
+++ JavaScript Projects CodingNepal/39 Create A Dynamic Calendar/script.js
@@ -1,3 +1,8 @@
 const daysTag = document.querySelector(".days"),
   currentDate = document.querySelector(".current-date"),
   prevNextIcon = document.querySelectorAll(".icons span");
+
+// getting new date, current year and month
+let date = new Date(),
+  currYear = date.getFullYear(),
+  currMonth = date.getMonth();
