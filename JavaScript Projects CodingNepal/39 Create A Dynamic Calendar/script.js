880f7d5b0f392936972e0bc3f91aa6791b9bb2e0
mkkukul <mkkukul@gmail.com>
2024-07-01 21:28:17 +0300

    26

--- JavaScript Projects CodingNepal/39 Create A Dynamic Calendar/script.js
+++ JavaScript Projects CodingNepal/39 Create A Dynamic Calendar/script.js
@@ -6,6 +6,7 @@ const daysTag = document.querySelector(".days"),
 let date = new Date(),
   currYear = date.getFullYear(),
   currMonth = date.getMonth();
+
 // storing full name of all months in array
 const months = [
   "January",
@@ -21,16 +22,19 @@ const months = [
   "November",
   "December",
 ];
+
 const renderCalendar = () => {
   let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
     lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
     lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
     lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
   let liTag = "";
+
   for (let i = firstDayofMonth; i > 0; i--) {
     // creating li of previous month last days
     liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
   }
+
   for (let i = 1; i <= lastDateofMonth; i++) {
     // creating li of all days of current month
     // adding active class to li if the current day, month, and year matched
@@ -42,6 +46,7 @@ const renderCalendar = () => {
         : "";
     liTag += `<li class="${isToday}">${i}</li>`;
   }
+
   for (let i = lastDayofMonth; i < 6; i++) {
     // creating li of next month first days
     liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
@@ -50,16 +55,17 @@ const renderCalendar = () => {
   daysTag.innerHTML = liTag;
 };
 renderCalendar();
+
 prevNextIcon.forEach((icon) => {
   // getting prev and next icons
   icon.addEventListener("click", () => {
     // adding click event on both icons
     // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
     currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
     if (currMonth < 0 || currMonth > 11) {
       // if current month is less than 0 or greater than 11
-      // creating a new date of current year & month and pass it as
-
+      // creating a new date of current year & month and pass it as date value
       date = new Date(currYear, currMonth, new Date().getDate());
       currYear = date.getFullYear(); // updating current year with new date year
       currMonth = date.getMonth(); // updating current month with new date month
