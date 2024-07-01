0cdd455da6555122404a3363b8c9f8e206ee72a5
mkkukul <mkkukul@gmail.com>
2024-07-01 21:27:24 +0300

    25

--- JavaScript Projects CodingNepal/39 Create A Dynamic Calendar/script.js
+++ JavaScript Projects CodingNepal/39 Create A Dynamic Calendar/script.js
@@ -50,16 +50,22 @@ const renderCalendar = () => {
   daysTag.innerHTML = liTag;
 };
 renderCalendar();
-prevNextIcon.forEach(icon => { // getting prev and next icons
-    icon.addEventListener("click", () => { // adding click event on both icons
-        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
-        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
-        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-            // creating a new date of current year & month and pass it as
+prevNextIcon.forEach((icon) => {
+  // getting prev and next icons
+  icon.addEventListener("click", () => {
+    // adding click event on both icons
+    // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+    currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+    if (currMonth < 0 || currMonth > 11) {
+      // if current month is less than 0 or greater than 11
+      // creating a new date of current year & month and pass it as
 
-            date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear(); // updating current year with new date year
-            currMonth = date.getMonth(); // updating current month with new date month
-        } else {
-            date = new Date(); // pass the current date as date value
-        }
+      date = new Date(currYear, currMonth, new Date().getDate());
+      currYear = date.getFullYear(); // updating current year with new date year
+      currMonth = date.getMonth(); // updating current month with new date month
+    } else {
+      date = new Date(); // pass the current date as date value
+    }
+    renderCalendar(); // calling renderCalendar function
+  });
+});
