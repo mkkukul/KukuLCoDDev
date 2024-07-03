118651b43d576d66a5979c2b365d6993890ac860
mkkukul <mkkukul@gmail.com>
2024-07-03 06:24:33 +0300

    19

--- JavaScript Projects CodingNepal/40 Build A Drawing or Paint App/script.js
+++ JavaScript Projects CodingNepal/40 Build A Drawing or Paint App/script.js
@@ -1,3 +1,9 @@
 const canvas = document.querySelector("canvas"),
-toolBtns = document.querySelectorAll(".tool"),
-fillColor = document.querySelector("#fill-color"),
+  toolBtns = document.querySelectorAll(".tool"),
+  fillColor = document.querySelector("#fill-color"),
+  sizeSlider = document.querySelector("#size-slider"),
+  colorBtns = document.querySelectorAll(".colors .option"),
+  colorPicker = document.querySelector("#color-picker"),
+  clearCanvas = document.querySelector(".clear-canvas"),
+  saveImg = document.querySelector(".save-img"),
+  ctx = canvas.getContext("2d");
