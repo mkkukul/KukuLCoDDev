78f9fb3de025184592b6a97122d370078de99dc9
mkkukul <mkkukul@gmail.com>
2024-07-09 05:26:59 +0300

    26

--- JavaScript Projects CodingNepal/42/script.js
+++ JavaScript Projects CodingNepal/42/script.js
@@ -7,3 +7,5 @@ selectMenu.addEventListener("change", () => {
   const selectedFormat = selectMenu.options[selectMenu.selectedIndex].text;
   saveBtn.innerText = `Save As ${selectedFormat.split(" ")[0]} File`;
 });
+saveBtn.addEventListener("click", () => {
+    const blob = new Blob([textarea.value], {type: selectMenu.value});
