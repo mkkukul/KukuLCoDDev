58a4c22946d98e741e1a2925c5d03f9759dc7da7
mkkukul <mkkukul@gmail.com>
2024-07-09 05:26:24 +0300

    24

--- JavaScript Projects CodingNepal/42/script.js
+++ JavaScript Projects CodingNepal/42/script.js
@@ -2,3 +2,6 @@ const textarea = document.querySelector("textarea"),
   fileNameInput = document.querySelector(".file-name input"),
   selectMenu = document.querySelector(".save-as select"),
   saveBtn = document.querySelector(".save-btn");
+
+  selectMenu.addEventListener("change", () => {
+    const selectedFormat = 
