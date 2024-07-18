9f70129e6e9bcad816f7ad58b443d7d82e3b899b
mkkukul <mkkukul@gmail.com>
2024-07-18 22:06:13 +0300

    19

--- JavaScript Projects CodingNepal/43 How to Validate Email and Password/script.js
+++ JavaScript Projects CodingNepal/43 How to Validate Email and Password/script.js
@@ -6,9 +6,11 @@ const form = document.querySelector("form"),
   cPassField = form.querySelector(".confirm-password"),
   cPassInput = cPassField.querySelector(".cPassword");
 
-  // Email Validtion
+// Email Validtion
 function checkEmail() {
-    const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-    if (!emailInput.value.match(emaiPattern)) {
-        return emailField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
-      }
+  const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  if (!emailInput.value.match(emaiPattern)) {
+    return emailField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
+  }
+  emailField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
+}
