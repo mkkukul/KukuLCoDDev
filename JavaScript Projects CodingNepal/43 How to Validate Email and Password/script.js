e63d8c602073ccc562d472fd674f84e2008e842d
mkkukul <mkkukul@gmail.com>
2024-07-18 22:05:24 +0300

    14

---
+++ JavaScript Projects CodingNepal/43 How to Validate Email and Password/script.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("form"),
+  emailField = form.querySelector(".email-field"),
+  emailInput = emailField.querySelector(".email"),
+  passField = form.querySelector(".create-password"),
