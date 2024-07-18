6fd5c4c660919697b819fef7db38645463f866b0
mkkukul <mkkukul@gmail.com>
2024-07-18 22:06:56 +0300

    22

--- JavaScript Projects CodingNepal/43 How to Validate Email and Password/script.js
+++ JavaScript Projects CodingNepal/43 How to Validate Email and Password/script.js
@@ -19,4 +19,10 @@ function checkEmail() {
 const eyeIcons = document.querySelectorAll(".show-hide");
 eyeIcons.forEach((eyeIcon) => {
     eyeIcon.addEventListener("click", () => {
-      const pInput = eyeIcon.parentElement.querySelector("input"); 
+      const pInput = eyeIcon.parentElement.querySelector("input");
+      //getting parent element of eye icon and selecting the password input
+    if (pInput.type === "password") {
+        eyeIcon.classList.replace("bx-hide", "bx-show");
+        return (pInput.type = "text");
+      }
+
