933ef150c9e9662d2481cdcd09e23aca7dcd1c17
mkkukul <mkkukul@gmail.com>
2024-07-19 21:24:33 +0300

    20

--- JavaScript Projects CodingNepal/44 Word Scramble Game/script.js
+++ JavaScript Projects CodingNepal/44 Word Scramble Game/script.js
@@ -3,4 +3,15 @@ let words = [
         word: "addition",
         hint: "The process of adding numbers"
     },
-    
+    {
+        word: "meeting",
+        hint: "Event in which people come together"
+    },
+    {
+        word: "number",
+        hint: "Math symbol used for counting"
+    },
+    {
+        word: "exchange",
+        hint: "The act of trading"
+    },
