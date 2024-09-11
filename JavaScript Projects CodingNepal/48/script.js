61bccd1ec4d439ca766d9a611e37a13fdc247370
mkkukul <mkkukul@gmail.com>
2024-09-11 21:24:58 +0300

    19

---
+++ JavaScript Projects CodingNepal/48/script.js
@@ -0,0 +1,14 @@
+const selectBox = document.querySelector(".select-box"),
+  selectBtnX = selectBox.querySelector(".options .playerX"),
+  selectBtnO = selectBox.querySelector(".options .playerO"),
+  playBoard = document.querySelector(".play-board"),
+  players = document.querySelector(".players"),
+  allBox = document.querySelectorAll("section span"),
+  resultBox = document.querySelector(".result-box"),
+  wonText = resultBox.querySelector(".won-text"),
+  replayBtn = resultBox.querySelector("button");
+window.onload = () => {
+  for (let i = 0; i < allBox.length; i++) {
+    allBox[i].setAttribute("onclick", "clickedBox(this)");
+  }
+};
