722c4d640b0cb0987174789782ceeb34213a0417
mkkukul <mkkukul@gmail.com>
2024-09-12 04:57:11 +0300

    4

--- JavaScript Projects CodingNepal/48/script.js
+++ JavaScript Projects CodingNepal/48/script.js
@@ -21,3 +21,7 @@ selectBtnO.onclick = () => {
   playBoard.classList.add("show");
   players.setAttribute("class", "players active player");
 };
+let playerXIcon = "fas fa-times",
+  playerOIcon = "far fa-circle",
+  playerSign = "X",
+  runBot = true;
