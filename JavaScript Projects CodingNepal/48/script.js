f4480563e20c561bf6acfe5d32ed77af5a3fd67c
mkkukul <mkkukul@gmail.com>
2024-09-12 04:58:04 +0300

    7

--- JavaScript Projects CodingNepal/48/script.js
+++ JavaScript Projects CodingNepal/48/script.js
@@ -72,3 +72,15 @@ function bot() {
     playerSign = "X";
   }
 }
+function getIdVal(classname) {
+  return document.querySelector(".box" + classname).id;
+}
+function checkIdSign(val1, val2, val3, sign) {
+  if (
+    getIdVal(val1) == sign &&
+    getIdVal(val2) == sign &&
+    getIdVal(val3) == sign
+  ) {
+    return true;
+  }
+}
