4b2c711d7a416909cc00a1c90ff0214597aeb454
mkkukul <mkkukul@gmail.com>
2024-09-14 20:39:22 +0300

    10

--- JavaScript Projects  Coddig Artist/70 Detect Battery Status With Javascript/script.js
+++ JavaScript Projects  Coddig Artist/70 Detect Battery Status With Javascript/script.js
@@ -1,3 +1,11 @@
 const chargeLevel = document.getElementById("charge-level");
 const charge = document.getElementById("charge");
 const chargingTimeRef = document.getElementById("charging-time");
+
+window.onload = () => {
+  //For browsers that don't support the battery status API
+  if (!navigator.getBattery) {
+    alert("Battery Status Api Is Not Supported In Your Browser");
+    return false;
+  }
+};
