e273470b9273ef4a0e6516c3135827f0e10dd40c
mkkukul <mkkukul@gmail.com>
2024-09-14 20:40:12 +0300

    14

--- JavaScript Projects  Coddig Artist/70 Detect Battery Status With Javascript/script.js
+++ JavaScript Projects  Coddig Artist/70 Detect Battery Status With Javascript/script.js
@@ -28,3 +28,12 @@ navigator.getBattery().then((battery) => {
       charge.classList.add("active");
       chargingTimeRef.innerText = "";
     } else {
+        charge.classList.remove("active");
+      //Display time left to discharge only when it is a integer value i.e not infinity
+      if (parseInt(battery.dischargingTime)) {
+        let hr = parseInt(battery.dischargingTime / 3600);
+        let min = parseInt(battery.dischargingTime / 60 - hr * 60);
+        chargingTimeRef.innerText = `${hr}hr ${min}mins remaining`;
+      }
+    }
+  }
