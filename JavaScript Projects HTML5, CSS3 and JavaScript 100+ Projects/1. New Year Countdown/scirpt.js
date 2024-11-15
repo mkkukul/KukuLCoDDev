8181293e331847186930bca279e2a9a47e2ea769
mkkukul <mkkukul@gmail.com>
2024-11-15 21:10:57 +0300

    13

--- JavaScript Projects HTML5, CSS3 and JavaScript 100+ Projects/1. New Year Countdown/scirpt.js
+++ JavaScript Projects HTML5, CSS3 and JavaScript 100+ Projects/1. New Year Countdown/scirpt.js
@@ -1,2 +1,4 @@
 let dayBox = document.getElementById("day-box");
 let hrBox = document.getElementById("hr-box");
+let minBox = document.getElementById("min-box");
+let secBox = document.getElementById("sec-box");
